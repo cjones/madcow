408ddd617d92dea9451733d8571f1f6712291c54
Chris Jones <cjones@gmail.com>
2007-07-01 04:32:47 +0000

    Bring template into the modern world. improve documentation

--- modules/template.py
+++ modules/template.py
@@ -2,8 +2,8 @@
 
 """
 NOTE: This is the template for creating your own user modules.  Copy this file
-to something unique (it must end with .py!).  Edit the __init__ object.  This is
-where you set what regular expression will trigger the process() function when run
+to something unique (it must end with .py!).  Edit the __init__ function.  This is
+where you set what regular expression will trigger the process() call when run
 within the bot, whether it should spawn a new thread when running response(), and whether
 addressing is required.
 
@@ -16,6 +16,20 @@ import re
 # class for this module
 class match(object):
 	def __init__(self, config=None, ns='default', dir=None):
+		"""
+		arguments that this module gets:
+
+		config: the config class. this lets you access stuff in the .ini file
+
+		ns: namespace. if you are saving stuff to a DB file, you should name the file
+		     with this in it somewhere. this lets you run multiple bots from the same
+		     directory
+
+		dir: this is the directory the bot is run from. this is where you want
+		     to store any data files, if necessary
+
+		"""
+
 		self.enabled = True				# True/False - enabled?
 		self.pattern = re.compile('keyword\s+(\S+)')	# regular expression that needs to be matched
 		self.requireAddressing = True			# True/False - require addressing?
@@ -25,10 +39,33 @@ class match(object):
 
 	# function to generate a response
 	def response(self, *args, **kwargs):
+
+		"""
+		kwargs is a dict passed from the bot. this allows you to develop
+		more advanced modules that require introspection. the following
+		key/value pairs are made available:
+
+		<string>  nick		- nickname of the user invoking the module
+		<string>  channel	- channel that module was triggered from
+		<boolean> addressed	- True/False, whether bot was addressed by name
+		<boolean> correction	- True/False, whether user was correcting the bot
+		<list>    args		- list of args trapped from the regex above.
+		"""
+
 		nick = kwargs['nick']
 		args = kwargs['args']
+
 		try:
-			pass
+			"""
+			Your methods go here. You should return a string when you are done,
+			or None for no response.  Any excptions will be caught and
+			return an error. This is not strictly necessary, the bot will not
+			crash if you don't catch these errors. If you want it to silently
+			fail, you can remove this try/catch block.
+			"""
+
+			return None
+
 		except Exception, e:
 			print >> sys.stderr, 'error in %s: %s' % (self.__module__, e)
 			return '%s: I failed to perform that lookup' % nick
