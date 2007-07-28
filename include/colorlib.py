e48bc25474adf0ccbbd2241669719465f694186e
Chris Jones <cjones@gmail.com>
2007-07-28 01:44:07 +0000

    check in revised/reformatted colorlib

--- include/colorlib.py
+++ include/colorlib.py
@@ -1,7 +1,11 @@
-__version__ = '0.1'
+"""
+Library for colorizing objects in ANSI, mIRC or HTML.
+"""
 
+__version__ = '0.3'
+__author__ = 'Christopher Jones <cjones@gruntle.org>'
 __copyright__ = """
-Copyright (C) 2007 Christopher Jones <cjones@insub.org>
+Copyright (C) 2007 Christopher Jones <cjones@gruntle.org>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -16,8 +20,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-
-(see LICENSE for full details)
 """
 
 
@@ -31,6 +33,7 @@ class ColorLibError(Exception):
     def __str__(self):
         return self.error
 
+
 class ColorTypeUnknown(ColorLibError):
     """
     Raised when the requested color handler is unknown
@@ -40,6 +43,7 @@ class UnknownColor(ColorLibError):
     """
     Raised when user tries to use an unknown color
     """
+
 
 class UnknownRainbowStyle(ColorLibError):
     """
@@ -51,29 +55,41 @@ class ColorLib(object):
     """
     Base class for color handling
     """
+
     colorMap = {
         'red':            { 'code': 'r', 'mirc': '5',  'ansi': '1',  'html': '#DF0009' },
-        'orange':        { 'code': 'o', 'mirc': '4',  'ansi': '1b', 'html': '#FF000B' },
-        'yellow':        { 'code': 'y', 'mirc': '7',  'ansi': '3',  'html': '#C9D000' },
-        'bright yellow':    { 'code': 'Y', 'mirc': '8',  'ansi': '3b', 'html': '#FBFF00' },
-        'green':        { 'code': 'g', 'mirc': '3',  'ansi': '2',  'html': '#00D100' },
-        'bright green':        { 'code': 'G', 'mirc': '9',  'ansi': '2b', 'html': '#00FF00' },
-        'cyan':            { 'code': 'c', 'mirc': '10', 'ansi': '6',  'html': '#00CECC' },
-        'bright cyan':        { 'code': 'C', 'mirc': '11', 'ansi': '6b', 'html': '#00FFFE' },
-        'blue':            { 'code': 'b', 'mirc': '2',  'ansi': '4',  'html': '#1A00D1' },
-        'bright blue':        { 'code': 'B', 'mirc': '12', 'ansi': '4b', 'html': '#2100FF' },
+        'orange':         { 'code': 'o', 'mirc': '4',  'ansi': '1b', 'html': '#FF000B' },
+        'yellow':         { 'code': 'y', 'mirc': '7',  'ansi': '3',  'html': '#C9D000' },
+        'bright yellow':  { 'code': 'Y', 'mirc': '8',  'ansi': '3b', 'html': '#FBFF00' },
+        'green':          { 'code': 'g', 'mirc': '3',  'ansi': '2',  'html': '#00D100' },
+        'bright green':   { 'code': 'G', 'mirc': '9',  'ansi': '2b', 'html': '#00FF00' },
+        'cyan':           { 'code': 'c', 'mirc': '10', 'ansi': '6',  'html': '#00CECC' },
+        'bright cyan':    { 'code': 'C', 'mirc': '11', 'ansi': '6b', 'html': '#00FFFE' },
+        'blue':           { 'code': 'b', 'mirc': '2',  'ansi': '4',  'html': '#1A00D1' },
+        'bright blue':    { 'code': 'B', 'mirc': '12', 'ansi': '4b', 'html': '#2100FF' },
         'magenta':        { 'code': 'm', 'mirc': '6',  'ansi': '5',  'html': '#E200D2' },
-        'bright magenta':    { 'code': 'M', 'mirc': '13', 'ansi': '5b', 'html': '#FF00FF' },
+        'bright magenta': { 'code': 'M', 'mirc': '13', 'ansi': '5b', 'html': '#FF00FF' },
 
-        'black':        { 'code': '0', 'mirc': '1',  'ansi': '0',  'html': '#000000' },
-        'dark gray':        { 'code': '1', 'mirc': '14', 'ansi': '0b', 'html': '#808080' },
-        'light gray':        { 'code': '2', 'mirc': '15', 'ansi': '7',  'html': '#CCCCCC' },
-        'white':        { 'code': 'w', 'mirc': '0',  'ansi': '7b', 'html': '#FFFFFF' },
+        'black':          { 'code': '0', 'mirc': '1',  'ansi': '0',  'html': '#000000' },
+        'dark gray':      { 'code': '1', 'mirc': '14', 'ansi': '0b', 'html': '#808080' },
+        'light gray':     { 'code': '2', 'mirc': '15', 'ansi': '7',  'html': '#CCCCCC' },
+        'white':          { 'code': 'w', 'mirc': '0',  'ansi': '7b', 'html': '#FFFFFF' },
     }
 
     types = ('mirc', 'ansi', 'html')
     colors = colorMap.keys()
     codes = dict([(d['code'], color) for color, d in colorMap.items()])
+
+    rainbowmap = {
+        'rainbow': 'rryyggccbbmm',
+        'usa':     'ooowwwBBB',
+        'gray':    '111222',
+        'scale':   'ww22CC11CC22',
+        'xmas':    'rrgg',
+        'canada':  'ooowww',
+    }
+
+    rainbowStyles = rainbowmap.keys()
 
     def __init__(self, type=None):
         if type not in self.types:
@@ -82,7 +98,6 @@ class ColorLib(object):
         self.rainbowOffset = {}
         self.type = type
 
-
     def _normalizeColorName(self, color=None):
         if color is None: return
         color = color.lower()
@@ -90,12 +105,13 @@ class ColorLib(object):
         color = color.replace('bold', 'bright')
         color = color.replace('grey', 'gray')
         color = color.replace('purple', 'magenta')
-        if color == 'gray': color = 'light gray'
-        if len(color) == 1 and self.codes.has_key(color): color = self.codes[color]
+        if color == 'gray':
+            color = 'light gray'
+        if len(color) == 1 and self.codes.has_key(color):
+            color = self.codes[color]
         if self.colorMap.has_key(color) is False:
             raise UnknownColor, 'known colors: %s' % ', '.self.colors
         return color
-
 
     def getColor(self, fg=None, bg=None, type=None, nextChar=None):
         if type is None: type = self.type
@@ -147,7 +163,6 @@ class ColorLib(object):
 
             return '<span style="%s">' % ';'.join(codes)
 
-
     def reset(self, type=None):
         if type is None: type = self.type
         if type == 'ansi':
@@ -156,17 +171,6 @@ class ColorLib(object):
             return '\x0F'
         elif type == 'html':
             return '</span>'
-
-    rainbowmap = {
-        'rainbow':    'rryyggccbbmm',
-        'usa':        'ooowwwBBB',
-        'gray':        '111222',
-        'scale':    'ww22CC11CC22',
-        'xmas':        'rrgg',
-        'canada':    'ooowww',
-    }
-
-    rainbowStyles = rainbowmap.keys()
 
     def rainbow(self, text=None, offset=None, style='rainbow', bg=None, colorWhitespace=False):
         if text is None: return
