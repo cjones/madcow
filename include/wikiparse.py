c79693a8cee03a0f9308df2c9277c4c31cf6ae74
Chris Jones <cjones@gmail.com>
2007-11-08 04:21:11 +0000

    Redo WikiPedia with external libraries that use BeautifulSoup to scrape

---
+++ include/wikiparse.py
@@ -0,0 +1,101 @@
+"""Library to parse WikiPedia pages"""
+
+from BeautifulSoup import BeautifulSoup
+import urllib, urllib2, cookielib
+import re
+
+__version__ = '0.1'
+__author__ = 'Chris Jones <cjones@gruntle.org>'
+__license__ = 'GPL'
+
+
+class WikiParser(object):
+    """Class that represents a WikiPedia page"""
+    # constants
+    AGENT = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'
+    BASEURL = 'http://en.wikipedia.org/'
+    ADVERT = ' - Wikipedia, the free encyclopedia'
+    SUMMARY_SIZE = 400
+
+    # precompiled regex's
+    UTF8 = re.compile(r'[\x80-\xff]')
+    MARKUP = re.compile(r'<.*?>', re.DOTALL)
+    CITATIONS = re.compile(r'\[.*?\]', re.DOTALL)
+    AUDIO = re.compile(r'audiolink', re.I)
+    PARENS = re.compile(r'\(.*?\)', re.DOTALL)
+    WHITESPACE = re.compile(r'[ \t\r\n]+')
+    SENTENCE = re.compile(r'(.*?\.)\s+', re.DOTALL)
+
+    def __init__(self, query):
+        if isinstance(query, (list, tuple)):
+            query = ' '.join(query)
+        self.query = query
+
+        # load page, mimic a browser to trick their anti-bot stuff
+        cj = cookielib.CookieJar()
+        ch = urllib2.HTTPCookieProcessor(cj)
+        opener = urllib2.build_opener(ch)
+        opener.addheaders = [('User-Agent', WikiParser.AGENT)]
+        opts = {'search': self.query, 'go': 'Go'}
+        url = WikiParser.BASEURL + 'wiki/Special:Search'
+        req = urllib2.Request(url, urllib.urlencode(opts))
+        req.add_header('Referer', WikiParser.BASEURL)
+        res = opener.open(req)
+        page = res.read()
+
+        # remove high ascii from final page, this is going out to IRC
+        page = WikiParser.UTF8.sub('', page)
+
+        # create BeautifulSoup document tree
+        soup = BeautifulSoup(page)
+
+        # extract title
+        self.title = soup.title.string.replace(WikiParser.ADVERT, '')
+
+        # remove all tabular data/sidebars
+        for table in soup.findAll('table'):
+            table.extract()
+
+        # remove disambiguation links
+        for dablink in soup.findAll('div', attrs={'class': 'dablink'}):
+            dablink.extract()
+
+        # remove latitude/longitude metadata for places
+        for coord in soup.findAll('span', attrs={'id': 'coordinates'}):
+            coord.extract()
+
+        # strip non-english content wrappers
+        for span in soup.findAll('span', attrs={'lang': True}):
+            span.extract()
+
+        # remove IPA pronounciation guidelines
+        for span in soup.findAll('span', attrs={'class': 'IPA'}):
+            span.extract()
+        for link in soup.findAll('a', text='IPA'):
+            link.extract()
+        for span in soup.findAll('span', attrs={'class': WikiParser.AUDIO}):
+            span.extract()
+
+        # massage into plain text by concatenating paragraphs
+        content = []
+        for para in soup.findAll('p'):
+            content.append(str(para))
+        content = ' '.join(content)
+
+        # clean up rendered text
+        content = WikiParser.MARKUP.sub('', content)      # strip HTML
+        content = WikiParser.CITATIONS.sub('', content)   # remove citations
+        content = WikiParser.PARENS.sub('', content)      # remove parenthesis
+        content = WikiParser.WHITESPACE.sub(' ', content) # compress whitespace
+        content = content.strip()                         # strip whitespace
+        self.content = content
+
+        # generate summary by adding as many sentences as possible before limit
+        summary = '%s -' % self.title
+        for sentence in WikiParser.SENTENCE.findall(self.content):
+            if len(summary) + 1 + len(sentence) > WikiParser.SUMMARY_SIZE:
+                break
+            summary += ' %s' % sentence
+        self.summary = summary
+
+
--- modules/wikipedia.py
+++ modules/wikipedia.py
@@ -1,85 +1,12 @@
-#!/usr/bin/env python
-
 """Get summary from WikiPedia"""
 
-import sys
 import re
-import os
-import cookielib
-import urllib, urllib2
-from include import utils
-from include.BeautifulSoup import BeautifulSoup
-
-
-class WikiPedia(object):
-    """WikiPedia parser object"""
-    BASEURL = 'http://en.wikipedia.org/'
-    ADVERT = 'Wikipedia, the free encyclopedia'
-    parse_title = re.compile('<title>(.*?) - ' + ADVERT + '</title>', re.I)
-    content = re.compile(r'<!-- start content -->(.*?)<!-- end content -->',
-            re.I + re.DOTALL)
-    linefeed = re.compile(r'[\r\n]+')
-    whitespace = re.compile(r'\s+')
-    sentence = re.compile(r'(.*?\.)\s+', re.DOTALL)
-    dablink = re.compile(r'<div[^>]+class="dablink".*?<\/div>', re.I+re.DOTALL)
-    table = re.compile(r'<table.*?<\/table>', re.I+re.DOTALL)
-    SUMMARY_SIZE = 400
-    ERROR = 'No page with that title exists'
-
-    def __init__(self, query, opener):
-        if isinstance(query, list):
-            query = ' '.join(query)
-        self.query = query
-        self.opener = opener
-        self.title = 'unknown'
-        self.summary = 'blah blah [citation needed]'
-        self.loadPage()
-
-    def loadPage(self):
-        opts = {'search': self.query, 'go': 'Go'}
-        url = WikiPedia.BASEURL + 'wiki/Special:Search'
-        req = urllib2.Request(url, urllib.urlencode(opts))
-        req.add_header('Referer', WikiPedia.BASEURL)
-        data = self.opener.open(req).read()
-        try:
-            soup = BeautifulSoup(data)
-            [table.extract() for table in soup.findAll('table')]
-            [span.extract() for span in soup.findAll('span', attrs={
-                'id': 'coordinates'})]
-
-            data = str(soup)
-            self.title = WikiPedia.parse_title.search(data).group(1)
-            content = WikiPedia.content.search(data).group(1)
-            content = WikiPedia.dablink.sub(' ', content)
-            content = WikiPedia.table.sub(' ', content)
-            content = utils.stripHTML(content)
-            content = WikiPedia.linefeed.sub(' ', content)
-            content = WikiPedia.whitespace.sub(' ', content)
-        except:
-            pass
-
-        lines = WikiPedia.sentence.findall(content)
-        self.summary = '%s -' % self.title
-        for line in lines:
-            line = line.strip()
-            if len(self.summary) + 1 + len(line) > WikiPedia.SUMMARY_SIZE:
-                break
-            self.summary += ' %s' % line
-
-        self.content = content
-
-        if self.title == 'Search' and WikiPedia.ERROR in self.summary:
-            raise Exception, "couldn't find a page for %s" % repr(self.query)
+from include.wikiparse import WikiParser
 
 
 class MatchObject(object):
-    """This object is autoloaded by the bot"""
-    AGENT = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'
 
-    def __init__(self, config=None, ns='madcow', dir='..'):
-        self.ns = ns
-        self.dir = dir
-        self.config = config
+    def __init__(self, *args, **kwargs):
         self.enabled = True
         self.pattern = re.compile('^\s*(?:wp|wiki|wikipedia)\s+(.*?)\s*$', re.I)
         self.requireAddressing = True
@@ -87,29 +14,12 @@ class MatchObject(object):
         self.wrap = False
         self.help = 'wiki <term> - look up summary of term on wikipedia'
 
-        # build agent
-        cj = cookielib.CookieJar()
-        ch = urllib2.HTTPCookieProcessor(cj)
-        opener = urllib2.build_opener(ch)
-        opener.addheaders = [('User-Agent', MatchObject.AGENT)]
-        self.opener = opener
-
     def response(self, **kwargs):
-        """This function should return a response to the query or None. """
-
-        nick = kwargs['nick']
-        args = kwargs['args']
+        """Return summary of WikiPedia page"""
 
         try:
-            wp = WikiPedia(query=args[0], opener=self.opener)
-            return wp.summary
-
+            return WikiParser(query=kwargs['args']).summary
         except Exception, e:
-            return '%s: problem with query: %s' % (nick, e)
+            return '%s: problem with query: %s' % (kwargs['nick'], e)
 
 
-if __name__ == '__main__':
-    mo = MatchObject()
-    res = mo.response(nick=os.environ['USER'], args=[' '.join(sys.argv[1:])])
-    print res
-    sys.exit(0)
