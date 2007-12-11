a23a1dd41e35022f78a9178c557520cd6d06847a
Chris Jones <cjones@gmail.com>
2007-12-11 19:00:20 +0000

    add some comments to make the source a little more understandable ;p

--- include/email_gateway.py
+++ include/email_gateway.py
@@ -9,9 +9,14 @@ import email
 import mimetypes
 
 def main():
+    # static settings
     send_to = '#hugs'
+    madcow_service = ('localhost', 5000)
+
+    # get message from STDIN (for use with procmail)
     payload = sys.stdin.read()
 
+    # XXX save copy of message body for debugging purposes
     #"""
     import time
     filename = '/tmp/email-%s.txt' % int(time.time())
@@ -20,19 +25,20 @@ def main():
     fi.close()
     #"""
 
+    # regexes for parsing
     tags = re.compile(r'<[^>]+>')
     br = re.compile(r'<br[^>]*>')
-    quoted = re.compile(
-            r'^(---+)\s*(original|forwarded)\s+(message|email)\s*\1', re.I)
+    quoted = re.compile(r'^(---+)\s*(original|forwarded)\s+(message|email)\s*\1', re.I)
     is_header = re.compile(r'^\S+:\s*.+$', re.I)
     telus_spam = 'This is an MMS message. Please go to http://mms.telusmobility.com/do/LegacyLogin to view the message.'
 
+    # iterate over mime parts and extract likely message payloads
     msg = email.message_from_string(payload)
     plain = html = None
     for part in msg.walk():
         if part.get_content_maintype() == 'multipart':
             continue
-        filename = part.get_filename()
+        #filename = part.get_filename()
         mime_type = part.get_content_type()
         body = part.get_payload(decode=True)
 
@@ -42,6 +48,7 @@ def main():
         if mime_type == 'text/html':
             html = body
 
+    # use plain format if it exists, otherwise clean up html
     if plain:
         doc = plain
     elif html:
@@ -54,11 +61,11 @@ def main():
     else:
         doc = 'no message'
 
+    # strip out undesirable cruft like sigs, quoted messages
     nosig = []
     reading_quoted = False
     for line in doc.splitlines():
         line = line.strip()
-        #print '>>> %s' % repr(line)
         if not len(line):
             continue
         if line.startswith('>'):
@@ -74,20 +81,20 @@ def main():
         if line == '--':
             break
         nosig.append(line)
-
     message = ' '.join(nosig)
 
+    # remove spam from Telus carrier
     if telus_spam in message:
         message = message.replace(telus_spam, '')
 
-    message = message.strip()
-
+    # construct payload for madcow
     output = 'to: %s\n' % send_to
     output += 'from: %s\n' % msg['from']
-    output += 'message: %s' % message
+    output += 'message: %s' % message.strip()
 
+    # send message to madcow service
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('127.0.0.1', 5000))
+    s.connect(madcow_service)
     s.send(output)
     s.close()
 
