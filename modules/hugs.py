d54496a1a8648eff573eb6ebadcdbd0cf47c2692
Chris Jones <cjones@gmail.com>
2007-12-21 23:00:25 +0000

    bypass browser detection

--- modules/hugs.py
+++ modules/hugs.py
@@ -6,10 +6,11 @@ Get a random confession from grouphug.us
 
 import sys
 import re
-import urllib
+import urllib, urllib2, cookielib
 from include import utils
 import os
 
+AGENT = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'
 
 class MatchObject(object):
 
@@ -23,6 +24,13 @@ class MatchObject(object):
 
         self.confs = re.compile('<p>(.*?)</p>', re.I + re.DOTALL)
 
+        # build opener to mimic browser
+        cj = cookielib.CookieJar()
+        ch = urllib2.HTTPCookieProcessor(cj)
+        opener = urllib2.build_opener(ch)
+        opener.addheaders = [('User-Agent', AGENT)]
+        self.opener = opener
+
     def response(self, **kwargs):
         nick = kwargs['nick']
         args = kwargs['args']
@@ -33,7 +41,10 @@ class MatchObject(object):
             else:
                 url = 'http://grouphug.us/random'
 
-            doc = urllib.urlopen(url).read()
+            # load page
+            req = urllib2.Request(url)
+            res = self.opener.open(req)
+            doc = res.read()
 
             conf = self.confs.findall(doc)[0]
             conf = utils.stripHTML(conf)
