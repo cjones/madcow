e61cd1ba45dfc85945e2f36d09bb99a10726d0fb
Chris Jones <cjones@gmail.com>
2008-03-08 01:28:26 +0000

    add disambiguation parsing

--- modules/weather.py
+++ modules/weather.py
@@ -22,6 +22,9 @@ class Weather(object):
     _base_url = 'http://www.wunderground.com/'
     _search_url = urljoin(_base_url, '/cgi-bin/findweather/getForecast')
     _rss_link = {'type': 'application/rss+xml'}
+    _tempF = re.compile('(-?[0-9.]+)\s*\xb0\s*F', re.I)
+    _bar = re.compile(r'\s*\|\s*')
+    _keyval = re.compile(r'^\s*(.*?)\s*:\s*(.*?)\s*$')
 
     def __init__(self):
         self.ua = UserAgent()
@@ -30,11 +33,64 @@ class Weather(object):
         page = self.ua.fetch(url=Weather._search_url, opts={'query': location},
                 referer=Weather._base_url, method='GET')
         soup = BeautifulSoup(page)
+
+        # disambiguation page
+        if 'Search Results' in str(soup):
+            table = soup.find('table', attrs={'class': 'boxB full'})
+            rows = table.findAll('tr')
+            results = []
+            for row in rows:
+                cells = row.findAll('td', attrs={'class': 'sortC'})
+                for cell in cells:
+                    link = cell.find('a')
+                    if link is None or 'addfav' in str(link['href']):
+                        continue
+                    city = str(link.contents[0])
+                    href = urljoin(Weather._base_url, str(link['href']))
+                    results.append(city)
+            return 'Multiple results found: %s' % ', '.join(results)
+
         rss_url = soup.find('link', attrs=Weather._rss_link)['href']
         rss = rssparser.parse(rss_url)
         title = str(soup.find('h1').string).strip()
         conditions = stripHTML(rss['items'][0]['description'])
-        return '%s: %s' % (title, conditions)
+        fields = Weather._bar.split(conditions)
+        data = {}
+        for field in fields:
+            try:
+                key, val = Weather._keyval.search(field).groups()
+                data[key] = val
+            except:
+                pass
+
+        try:
+            temp = float(Weather._tempF.search(data['Temperature']).group(1))
+            if temp < 0:
+                color = 6
+            elif temp >=0 and temp <= 40:
+                color = 2
+            elif temp >= 40 and temp <= 60:
+                color = 10
+            elif temp >= 60 and temp <= 80:
+                color = 3
+            elif temp >= 80 and temp <= 100:
+                color = 7
+            elif temp > 100:
+                color = 5
+            data['Temperature'] = '\x03%s\x16\x16%s\x0F' % (color,
+                    data['Temperature'])
+
+        except:
+            pass
+
+        output = []
+        for key, val in data.items():
+            line = '%s: %s' % (key, val)
+            output.append(line)
+
+        output = ' | '.join(output)
+
+        return '%s: %s' % (title, output)
 
 
 class MatchObject(object):
@@ -73,7 +129,7 @@ class MatchObject(object):
         try:
             return '%s: %s' % (nick, self.weather.forecast(query))
         except Exception, e:
-            return '%s: problem with query: %s' % (nick, e)
+            return "Couldn't find that place, maybe a bomb dropped on it"
 
 
 if __name__ == '__main__':
