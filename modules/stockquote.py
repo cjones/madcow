59f159dc6c915598db8843d40f06121530c14b1f
Chris Jones <cjones@gmail.com>
2008-03-08 00:03:11 +0000

    colorize stock changes, and calculate percentage

--- modules/stockquote.py
+++ modules/stockquote.py
@@ -18,6 +18,10 @@ _dir = '..'
 
 class Yahoo(Base):
     _quote_url = 'http://finance.yahoo.com/q?s=SYMBOL'
+    _isfloat = re.compile(r'^\s*-?\s*[0-9.,]+\s*$')
+    _green = '\x039'
+    _red = '\x035'
+    _reset = '\x0F'
 
     def __init__(self):
         self.ua = UserAgent()
@@ -31,7 +35,9 @@ class Yahoo(Base):
         tables = soup.findAll('table')
         table = tables[0]
         rows = table.findAll('tr')
-        data = []
+        data = {}
+        current_value = 0.0
+        open_value = 0.0
         for row in rows:
             key, val = row.findAll('td')
             key = str(key.contents[0])
@@ -44,9 +50,42 @@ class Yahoo(Base):
             else:
                 val = stripHTML(str(val.contents[0]))
 
-            data.append('%s %s' % (key, val))
+            if Yahoo._isfloat.search(val):
+                val = val.replace(',', '')
+                val = float(val)
 
-        return '%s - ' % company + ', '.join(data)
+            data[key] = val
+
+            if key == 'Last Trade:' or key == 'Index Value:':
+                current_value = val
+
+            elif key == 'Open:':
+                open_value = val
+
+        # see if we can calculate percentage
+        try:
+            change = 100 * (current_value - open_value) / open_value
+            data['Change:'] += ' (%.2f%%)' % change
+        except:
+            pass
+
+        # try and colorize the change field
+        try:
+            if 'Up' in data['Change:']:
+                data['Change:'] = self._green + data['Change:'] + self._reset
+            elif 'Down' in data['Change:']:
+                data['Change:'] = self._red + data['Change:'] + self._reset
+        except:
+            pass
+
+        # build friendly output
+        output = []
+        for key, val in data.items():
+            if isinstance(val, float):
+                val = '%.2f' % val
+            output.append('%s %s' % (key, val))
+
+        return '%s - ' % company + ', '.join(output)
 
 
 class MatchObject(Base):
