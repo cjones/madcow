78b0eb8aef80361cff80af37d7ce4e220a272807
Chris Jones <cjones@gmail.com>
2008-03-08 23:23:24 +0000

    fix case where some searches don't redirect

--- modules/google.py
+++ modules/google.py
@@ -1,43 +1,66 @@
 #!/usr/bin/env python
 
-"""
-I'm feeling lucky
-"""
+"""I'm feeling lucky"""
 
 import sys
-import re
 import urllib
 import urllib2
 import os
+from include.utils import Base
+from urlparse import urljoin
+import re
 
+__version__ = '0.2'
+__author__ = 'cj_ <cjones@gruntle.org>'
+__license__ = 'GPL'
 
 class NoRedirects(urllib2.HTTPRedirectHandler):
-    """
-    Override auto-follow of redirects
-    """
+    """Override auto-follow of redirects"""
 
-    def redirect_request(self, req, fp, code, msg, headers, newurl):
+    def redirect_request(self, *args, **kwargs):
         pass
 
 
 class NoErrors(urllib2.HTTPDefaultErrorHandler):    
-    """
-    Don't allow urllib to throw an error on 30x code
-    """
+    """Don't allow urllib to throw an error on 30x code"""
 
     def http_error_default(self, req, fp, code, msg, headers): 
         return dict(headers.items())['location']
 
 
-class MatchObject(object):
+class Google(Base):
+    _base_url = 'http://www.google.com/'
+    _search_url = urljoin(_base_url, '/search')
+    _agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'
+    _lucky_opts = {'hl': 'en', 'btnI': 'I', 'aq': 'f', 'safe': 'off'}
 
-    def __init__(self, config=None, ns='madcow', dir=None):
+    def __init__(self):
+        self.opener = urllib2.build_opener(NoRedirects, NoErrors)
+
+    def lucky(self, query):
+        opts = Google._lucky_opts
+        opts['q'] = query
+        url = Google._search_url + '?' + urllib.urlencode(opts)
+        request = urllib2.Request(url)
+        request.add_header('User-Agent', Google._agent)
+        request.add_header('Referer', Google._base_url)
+        result = self.opener.open(request)
+        if isinstance(result, str):
+            return '%s = %s' % (query, result)
+        else:
+            raise Exception('No matches for ' + query)
+
+
+class MatchObject(Base):
+
+    def __init__(self, *args, **kwargs):
         self.enabled = True
-        self.pattern = re.compile('^\s*google\s+(.+)')
+        self.pattern = re.compile('^\s*google\s+(.*?)\s*$')
         self.requireAddressing = True
         self.thread = True
         self.wrap = False
         self.help = "google <query> - i'm feeling lucky"
+        self.google = Google()
 
     def response(self, **kwargs):
         nick = kwargs['nick']
@@ -45,20 +68,15 @@ class MatchObject(object):
 
         try:
             query = args[0]
-            url = 'http://www.google.com/search?btnI=I&' + urllib.urlencode({'q' : query})
-            request = urllib2.Request(url)
-            request.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)')
-            result = urllib2.build_opener(NoRedirects, NoErrors).open(request)
-            if isinstance(result, str):
-                return '%s: %s = %s' % (nick, query, result)
-            else:
-                raise Exception('No matches for ' + query)
-            o
+            return '%s: %s' % (nick, self.google.lucky(query))
         except Exception, e:
             print >> sys.stderr, 'error in %s: %s' % (self.__module__, e)
             return '%s: Not so lucky today.. %s' % (nick, e)
 
 
 if __name__ == '__main__':
-    print MatchObject().response(nick=os.environ['USER'], args=[' '.join(sys.argv[1:])])
+    nick = os.environ['USER']
+    args = [' '.join(sys.argv[1:])]
+    mo = MatchObject()
+    print mo.response(nick=nick, args=args)
     sys.exit(0)
