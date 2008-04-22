3a9a88a5c4ca637e85c84b2eadf15282dc63bf00
Bryan Burns <bryanburns@gmail.com>
2008-04-22 03:22:23 +0000

    twitter module

---
+++ periodic/tweetprinter.py
@@ -0,0 +1,48 @@
+#  tweetprinter.py
+#  madcow
+#  
+#  Created by toast on 2008-04-21.
+# 
+# Periodically checks for fresh 'tweets' from friends and prints them to the channel
+#
+
+"""Prints tweets to the channel."""
+
+from include.utils import Base
+from include import twitter
+import time
+
+class PeriodicEvent(Base):
+
+  def __init__(self, madcow):
+    self.madcow = madcow
+    self.enabled = True
+    self.frequency = madcow.config.twitter.updatefreq
+    self.output = madcow.config.periodic.channel
+    self.__updatelast()
+  
+  def __updatelast(self):
+    """Updates timestamp of last update."""
+    self.lastupdate = time.strftime("%a, %d %b %Y %X GMT", time.gmtime())
+
+  def process(self):
+    """This is called by madcow, should return a string or None"""
+    print "checking for new tweets... last update is %s" % self.lastupdate
+    try:
+      tweets = twitter.Api().GetFriendsTimeline(user=self.madcow.config.twitter.username, since=self.lastupdate)
+    except Exception,e:
+      print str(e)
+      return None
+    finally:
+      self.__updatelast()
+    
+    lines = []
+    
+    for t in tweets:
+      line = "tweet from %s: %s" % (t.user.screen_name, t.text)
+      lines.append(line)
+    
+    if lines:
+      return "\n".join(lines)
+    else:
+      return None
