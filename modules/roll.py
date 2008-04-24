21cbe0242bc6e3bcdddc139ecb8eae6cdffa6db7
Chris Jones <cjones@gmail.com>
2008-04-24 00:07:45 +0000

    i can't believe i wasted time on this

--- modules/roll.py
+++ modules/roll.py
@@ -4,13 +4,15 @@
 
 import re
 import random
+import math
 
 class MatchObject(object):
-    _pattern = re.compile(r'^\s*roll\s+(-?[0-9.]+)d(-?[0-9.]+)\s*$', re.I)
+    _allow = '-?(?:[0-9.]+j?|pi|e)'
+    _regex = '^\s*roll\s+(%s)d(%s)\s*$' % (_allow, _allow)
 
     def __init__(self, *args, **kwargs):
         self.enabled = True
-        self.pattern = self._pattern
+        self.pattern = re.compile(self._regex, re.I)
         self.requireAddressing = True
         self.thread = False
         self.wrap = True
@@ -18,7 +20,7 @@ class MatchObject(object):
         random.seed()
 
     def roll(self, min, max):
-        if isinstance((min * max), float):
+        if isinstance((min * max), (float, complex)):
             return random.uniform(min, max)
         else:
             return random.randint(min, max)
@@ -27,14 +29,26 @@ class MatchObject(object):
         nick = kwargs['nick']
         args = kwargs['args']
 
-        num_dice = args[0]
-        if '.' in num_dice:
+        num_dice = args[0].lower()
+        if num_dice == 'pi':
+            num_dice = math.pi
+        elif num_dice == 'e':
+            num_dice = math.e
+        elif num_dice.endswith('j'):
+            num_dice = complex(num_dice)
+        elif '.' in num_dice:
             num_dice = float(num_dice)
         else:
             num_dice = int(num_dice)
 
-        sides = args[1]
-        if '.' in sides:
+        sides = args[1].lower()
+        if sides == 'pi':
+            sides = math.pi
+        elif sides == 'e':
+            sides = math.e
+        elif sides.endswith('j'):
+            sides = complex(sides)
+        elif '.' in sides:
             sides = float(sides)
         else:
             sides = int(sides)
@@ -49,10 +63,13 @@ class MatchObject(object):
         saving_throw = self.roll(min, max)
         save_versus = self.roll(min, max)
 
-        if saving_throw >= save_versus:
-            result = '\x033\x16\x16LIVES\x0f'
-        else:
-            result = '\x035\x16\x16DIES\x0f'
+        try:
+            if saving_throw >= save_versus:
+                result = '\x033\x16\x16LIVES\x0f'
+            else:
+                result = '\x035\x16\x16DIES\x0f'
+        except:
+            result = '\x037\x16\x16IS IN LIMBO\x0f'
 
         return '%s rolls %s, needs %s, %s %s' % (nick, saving_throw,
                 save_versus, nick, result)
