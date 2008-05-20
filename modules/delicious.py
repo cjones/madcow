1519dd62bf201cfd5a9434208a3b752cbf5c237d
Chris Jones <cjones@gmail.com>
2008-05-20 08:39:31 +0000

    fix delicious addon

--- modules/delicious.py
+++ modules/delicious.py
@@ -16,12 +16,13 @@ class Delicious(Base):
     posturl = urljoin(baseurl, '/v1/posts/add')
     opts = {
         'url': None,
-        'description': 'posted from madcow',
-        'tags': ['madcow'],
+        'description': None,
+        'tags': [],
         'replace': 'no',
         'shared': 'yes',
     }.items()
     code = re.compile(r'code="(.*?)"')
+    title = re.compile(r'<title>(.*?)</title>', re.I+re.DOTALL)
 
     def __init__(self, username, password):
         password_mgr = HTTPPasswordMgrWithDefaultRealm()
@@ -30,13 +31,18 @@ class Delicious(Base):
         self.ua = UserAgent(handlers=[auth_handler])
 
     def post(self, url, tags):
+        try:
+            html = self.ua.openurl(url, size=2048)
+            title = self.title.search(html).group(1)
+        except:
+            title = url
         opts = dict(self.opts)
         opts['url'] = url
+        opts['description'] = title
         opts['tags'] += tags
         opts['tags'] = ' '.join(opts['tags'])
         result = self.ua.openurl(self.posturl, opts=opts)
         code = self.code.search(result).group(1)
-        print >> sys.stderr, '%s -> delicious: %s' % (url, code)
 
 
 class Main(Module):
@@ -59,7 +65,7 @@ class Main(Module):
     def response(self, nick, args, **kwargs):
         try:
             for url in self.url.findall(args[0]):
-                self.delicious.post(url, nick)
+                self.delicious.post(url, tags=['madcow', nick])
         except Exception, e:
             print >> sys.stderr, 'error in %s: %s' % (self.__module__, e)
             return '%s: problem with query: %s' % (nick, e)
