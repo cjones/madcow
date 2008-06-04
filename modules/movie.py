caf95e0f83076c622503a5532cbc1b744c13706b
Chris Jones <cjones@gmail.com>
2008-06-04 00:44:24 +0000

    new and improved movie rating module

--- modules/movie.py
+++ modules/movie.py
@@ -5,23 +5,24 @@
 from include.utils import Module, Base
 import logging as log
 import re
-from include.useragent import geturl   # mimic browser
+from include.useragent import geturl
 from urlparse import urljoin
-from include.utils import stripHTML    # strip HTML/unescape entities
+from include.utils import stripHTML
 
-__version__ = '0.1'
+__version__ = '0.2'
 __author__ = 'cj_ <cjones@gmail.com>'
 __license__ = 'GPL'
 __copyright__ = 'Copyright (C) 2008 Christopher Jones'
-__all__ = []
+__all__ = ['IMDB', 'RottenTomatoes', 'MovieRatings']
 
-# global regex
+# global
 reopts = re.I + re.DOTALL
 whitespace = re.compile(r'\s+')
 html_title = re.compile(r'<title>(.*?)</title>', re.I)
 year = re.compile(r'\(\d{4}\)\s*$')
 
 class IMDB(Base):
+    """Interface to IMDB"""
     baseurl = 'http://www.imdb.com/'
     search = urljoin(baseurl, '/find')
     search_title = 'IMDb  Search'
@@ -30,6 +31,7 @@ class IMDB(Base):
     rating = re.compile(r'<b>User Rating:</b>.*<b>([0-9.]+)/10</b>', reopts)
 
     def rate(self, movie):
+        """Get the rating for a movie"""
         try:
             page = geturl(self.search, opts={'s': 'all', 'q': movie})
             title = html_title.search(page).group(1)
@@ -63,58 +65,76 @@ class IMDB(Base):
             response += ': %s' % rating
             return response
 
-        except Exception, e:
-            log.warn('error in %s: %s' % (self.__module__, e))
-            log.exception(e)
-            return 'IMDB: ?'
+        except:
+            return
 
 
 class RottenTomatoes(Base):
+    """Interface to Rotten Tomatoes"""
     baseurl = 'http://www.rottentomatoes.com/'
     search = urljoin(baseurl, '/search/search.php')
-    movies = re.compile('<a class=movie-link href="(.*?)"', reopts)
-    _rating = re.compile(r'<a onmouseover="toggle_display\(\'bubble_allCritics\'\)" onmouseout="toggle_display\(\'bubble_allCritics\'\)" title="(.*?%)"', reopts)
+    search_title = 'ROTTEN TOMATOES: Movie Reviews &amp; Previews'
+    movies = re.compile(r'<a class=movie-link href="(.*?)">(.*?)</a>', reopts)
+    movie_title = re.compile(r'<h1 class="movie_title">(.*?)</h1>', reopts)
+    rating = re.compile(r'<div id="bubble_allCritics".*?>\s*(\d+%)', reopts)
 
     def rate(self, movie):
+        """Get the freshness rating of a movie"""
         try:
             opts={'sitesearch': 'rt', 'search': movie}
             page = geturl(self.search, opts=opts, referer=self.baseurl)
-            print page
             title = html_title.search(page).group(1)
-            print title
+            if title == self.search_title:
+                # normalize search results
+                movies = self.movies.findall(page)
+                movies = [(path, normalize(title)) for path, title in movies]
 
-            #titleurl = self.titles.findall(html)[0]
-            #titleurl = urljoin(self.baseurl, titleurl)
-            #html = geturl(titleurl, referer=self.searchurl)
-            #rating = self._rating.search(html).group(1)
-            #return rating
-        except Exception, e:
-            log.warn('error in %s: %s' % (self.__module__, e))
-            log.exception(e)
-            return 'Freshness: ?'
+                # look for exact match
+                url = None
+                for path, title in movies:
+                    if title == movie:
+                        url = urljoin(self.baseurl, path)
+                        break
+
+                # no exact match, take first one
+                if not url:
+                    url = urljoin(self.baseurl, movies[0][0])
+
+                # load page
+                page = geturl(url, referer=self.search)
+
+            # find rating
+            title = self.movie_title.search(page).group(1)
+            rating = self.rating.search(page).group(1)
+
+            # construct response
+            response = 'Freshness'
+            if normalize(title) != movie:
+                response += ' [%s]' % stripHTML(title)
+            response += ': %s' % rating
+            return response
+
+        except:
+            return
 
 
 class MovieRatings(Base):
+    """Class that gets movie ratings from IMDB and Rotten Tomatoes"""
     imdb = IMDB()
     rt = RottenTomatoes()
 
     def rate(self, movie):
+        """Get movie ratings from imdb and rotten tomatoes"""
         movie = normalize(movie)
-        #ratings = [self.imdb.rate(movie), self.rt.rate(movie)]
-        ratings = [self.imdb.rate(movie)]
+        ratings = [self.imdb.rate(movie), self.rt.rate(movie)]
         ratings = [rating for rating in ratings if rating is not None]
-        return ', '.join(ratings)
+        if ratings:
+            return ', '.join(ratings)
+        return 'movie not found'
 
-
-def normalize(name):
-    name = stripHTML(name)
-    name = year.sub('', name)
-    name = name.lower()
-    name = name.strip()
-    name = whitespace.sub(' ', name)
-    return name
 
 class Main(Module):
+    """Autoloaded by MadCow"""
     pattern = re.compile(r'^\s*rate\s+(.+?)\s*$', re.I)
     error = 'does that movie even exist?'
     movie = MovieRatings()
@@ -127,6 +147,15 @@ class Main(Module):
             log.exception(e)
             return '%s: %s' % (nick, self.error)
 
+
+def normalize(name):
+    """Normalize a movie title for easy comparison"""
+    name = stripHTML(name)
+    name = year.sub('', name)
+    name = name.lower()
+    name = name.strip()
+    name = whitespace.sub(' ', name)
+    return name
 
 if __name__ == '__main__':
     from include.utils import test_module
