0eab2f828f147dc931ee5e403e780b23a43cfb9b
Chris Jones <cjones@gmail.com>
2008-06-06 06:15:26 +0000

    rewrite terrible lyrics modules

--- modules/lyrics.py
+++ modules/lyrics.py
@@ -1,146 +1,142 @@
 #!/usr/bin/env python
 
-"""Get lyrics from http://www.lyricsfreak.com/"""
+"""Look up song lyrics"""
 
+from include.utils import Module, Base
+import logging as log
 import re
-from include.utils import Base, stripHTML, Module
 from include.useragent import geturl
+from include.utils import stripHTML
 from urlparse import urljoin
 from include.BeautifulSoup import BeautifulSoup
 import random
-import logging as log
 
-__version__ = '0.2'
+__version__ = '0.1'
 __author__ = 'cj_ <cjones@gruntle.org>'
 __license__ = 'GPL'
-_namespace = 'madcow'
-_dir = '..'
+__copyright__ = 'Copyright (C) 2008 Christopher Jones'
+__all__ = []
 
-class Lyrics(Base):
-    _baseurl = 'http://www.lyricsfreak.com/'
-    _search = urljoin(_baseurl, '/search.php')
-    _artist_songs = urljoin(_baseurl, '/SECTION/ARTIST/lyrics.html')
-    _opts = {'type': 'title', 'sa.x': 21, 'sa.y': 20, 'sa': 'Search', 'q': ''}
-    _re_by = re.compile(r'\s*by\s*')
-    _re_dash = re.compile(r'\s*-\s*')
-    _links = {'title': re.compile(r'lyrics')}
+whitespace = re.compile(r'\s+')
+badchars = re.compile(r'[^a-z0-9 ]', re.I)
+
+class LyricsFreak(Base):
+    baseurl = 'http://www.lyricsfreak.com/'
+    search = urljoin(baseurl, '/search.php')
+    search_opts = {'sa.x': '0', 'sa.y': '0', 'sa': 'Search'}
+    result = re.compile(r'<td class="lyric">.*?<a href="(.*?)" title="(.*?)">',
+            re.I + re.DOTALL)
+
     _newline = re.compile(r'[\r\n]+')
-    _break = re.compile(r'<br(?:\s+/)?\s*>', re.I + re.DOTALL)
     _leadbreak = re.compile(r'^(?:<br(?:\s+/)?\s*>\s*)+', re.I + re.DOTALL)
     _endbreak = re.compile(r'(?:<br(?:\s+/)?\s*>\s*)+$', re.I + re.DOTALL)
-    _whitespace = re.compile(r'\s+')
-    _spam = '!! &nbsp; &nbsp;Download to your phone.\n'
+    _break = re.compile(r'<br(?:\s+/)?\s*>', re.I + re.DOTALL)
+    _spam = '!! &nbsp; &nbsp;Download to your phone.'
 
-    def get_lyrics(self, query):
-        # full lyrics or random verse?
-        if query[-1] == 'full':
-            full = True
-            query = query[:-1]
-        else:
-            full = False
+    def get_song_for_artist(self, qsong, qartist):
+        opts = dict(self.search_opts)
+        opts['type'] = 'artist'
+        opts['q'] = qartist
+        page = geturl(self.search, opts=opts, referer=self.baseurl)
+        results = self.result.findall(page)
+        exact = None
+        for url, artist in results:
+            if normalize(artist) == normalize(qartist):
+                exact = url
+                break
+        if not exact:
+            exact = results[0][0]
+        url = urljoin(self.baseurl, exact)
+        url = urljoin(url, 'lyrics.html')
+        page = geturl(url, referer=self.search)
+        results = self.result.findall(page)
+        exact = None
+        for url, song in results:
+            song = song.replace(' lyrics', '')
+            if normalize(song) == normalize(qsong):
+                exact = url
+                break
+        if not exact:
+            exact = results[0][0]
+        url = urljoin(self.baseurl, exact)
+        return self.get_lyrics_from_url(url)
 
-        song_url = None
+    def get_lyrics_from_url(self, url):
+        page = geturl(url, referer=self.baseurl)
+        soup = BeautifulSoup(page)
+        content = soup.find('div', attrs={'id': 'content'})
+        [div.extract() for div in content.findAll('div')]
+        [link.extract() for link in content.findAll('a')]
+        [script.extract() for script in content.findAll('script')]
+        lines = [str(line) for line in content.contents]
+        data = ''.join(lines)
+        data = self._newline.sub('', data)
+        data = self._leadbreak.sub('', data)
+        data = self._endbreak.sub('', data)
+        lines = self._break.split(data)
+        verses = []
+        while True:
+            try:
+                i = lines.index('')
+                verse, lines = lines[:i], lines[i+1:]
+                verses.append(verse)
+            except ValueError:
+                verses.append(lines)
+                break
+        for i, verse in enumerate(verses):
+            verse = ' / '.join(verse)
+            verse = whitespace.sub(' ', verse)
+            verses[i] = verse
+        if self._spam in verses:
+            del verses[verses.index(self._spam)]
+        return verses
 
-        # request for a specific song
-        if query[0] == 'song':
-            query = query[1:]
+    def search_for_song(self, song):
+        return ['not implemented yet, supply an artist']
 
-            if 'by' in query:
-                song, artist = Lyrics._re_by.split(' '.join(query))
-            else:
-                song = query
-                artist = None
-
-            url = Lyrics._search
-            opts = Lyrics._opts
-            opts['q'] = song
-
-            page = geturl(url=url, opts=opts)
-            soup = BeautifulSoup(page)
-
-            for cell in soup.findAll('td', attrs={'class': 'lyric'}):
-                link = cell.find('a')
-                link.find('b').extract()
-                cell_title = str(link.contents[0])
-                cell_artist, cell_title = Lyrics._re_dash.split(cell_title)
-                if artist is None or cell_artist.lower() == artist:
-                    song_url = str(link['href'])
-                    break
-
-        # request for a random song from an artist
-        else:
-            url = Lyrics._artist_songs
-            url = url.replace('SECTION', query[0][0])
-            url = url.replace('ARTIST', '+'.join(query))
-            page = geturl(url)
-            soup = BeautifulSoup(page)
-            songs = []
-            for cell in soup.findAll('td', attrs={'class': 'lyric'}):
-                link = cell.find('a')
-                link.find('b').extract()
-                cell_title = str(link.contents[0])
-                songs.append(str(link['href']))
-
-            if songs:
-                song_url = random.choice(songs)
-
-        if song_url:
-            page = geturl(song_url)
-            soup = BeautifulSoup(page)
-            content = soup.find('div', attrs={'id': 'content'})
-            [div.extract() for div in content.findAll('div')]
-            [link.extract() for link in content.findAll('a')]
-            [script.extract() for script in content.findAll('script')]
-            lines = [str(line) for line in content.contents]
-            data = ''.join(lines)
-            data = Lyrics._newline.sub('', data)
-            data = Lyrics._leadbreak.sub('', data)
-            data = Lyrics._endbreak.sub('', data)
-            lines = Lyrics._break.split(data)
-            verses = []
-            while True:
-                try:
-                    i = lines.index('')
-                    verse, lines = lines[:i], lines[i+1:]
-                    verses.append(verse)
-                except ValueError:
-                    verses.append(lines)
-                    break
-
-            for i, verse in enumerate(verses):
-                verse = ' / '.join(verse)
-                verse = Lyrics._whitespace.sub(' ', verse)
-                verses[i] = verse
-
-
-            if full:
-                response = '\n'.join(verses)
-            else:
-                response = random.choice(verses)
-            response = response.replace(self._spam, '')
-            return response
-
-        else:
-            return "Couldn't find a match for that query"
+    def random_song_for_artist(self, song):
+        return ['not implemented yet, supply a song name + artist']
 
 
 class Main(Module):
-    pattern = re.compile(r'^\s*sing\s+(.+)$')
-    require_addressing = True
-    help = 'sing (<artist>|song <song> [by <artist>]) [full] - lyrics'
+    r = r'^\s*sing\s+(?:song\s+(.+?)(?:\s+by\s+(.+?))?|(.+?))(?:\s+(full))?\s*$'
+    pattern = re.compile(r, re.I)
+    help = 'sing [song <song> [by <artist>]|<artist>] [full]'
+    error = "couldn't find that song :("
 
     def __init__(self, madcow=None):
-        self.lyrics = Lyrics()
+        self.freak = LyricsFreak()
 
     def response(self, nick, args, **kwargs):
-        query = args[0].lower().split()
         try:
-            return self.lyrics.get_lyrics(query)
+            song, artist, random_song, full = args
+            if song:
+                if artist:
+                    response = self.freak.get_song_for_artist(song, artist)
+                else:
+                    response = self.freak.search_for_song(song)
+            elif random_song:
+                response = self.freak.random_song_for_artist(random_song)
+            else:
+                response = 'what?'
+            if response:
+                if full:
+                    return '\n'.join(response)
+                else:
+                    return random.choice(response)
         except Exception, e:
             log.warn('error in %s: %s' % (self.__module__, e))
             log.exception(e)
-            return '%s: i had issues with that' % nick
+            return '%s: %s' % (nick, self.error)
 
+
+def normalize(name):
+    name = stripHTML(name)
+    name = badchars.sub('', name)
+    name = name.lower()
+    name = name.strip()
+    name = whitespace.sub(' ', name)
+    return name
 
 if __name__ == '__main__':
     from include.utils import test_module
