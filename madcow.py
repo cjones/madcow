aa6fca7c4dfae73fb5ed34d8aec207dffd215fe7
Chris Jones <cjones@gmail.com>
2008-11-12 09:42:01 +0000

    add megahal to configfile

--- madcow.py
+++ madcow.py
@@ -57,7 +57,7 @@ __all__ = [u'Madcow']
 MADCOW_URL = u'http://code.google.com/p/madcow/'
 CHARSET = u'utf-8'
 CONFIG = u'madcow.ini'
-SAMPLE_HASH = u'6cb4aba9f9c1473a3e3af0995276bd75'
+SAMPLE_HASH = u'4f6beec88995cb37e6132632a5c78e3b'
 LOG = dict(level=log.WARN, stream=sys.stderr, datefmt=u'%x %X',
            format=u'[%(asctime)s] %(levelname)s: %(message)s')
 
