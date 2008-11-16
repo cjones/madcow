7672b0a081e161f9c7a15c053c5704ea107ffb54
Chris Jones <cjones@gmail.com>
2008-11-16 08:52:30 +0000

    foo

--- include/halpy/create.sql
+++ include/halpy/create.sql
@@ -1,48 +1,119 @@
+-- MySQL dump 10.13  Distrib 5.1.29-rc, for portbld-freebsd6.4 (i386)
+--
+-- Host: localhost    Database: tree
+-- ------------------------------------------------------
+-- Server version	5.1.29-rc
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `backward`
+--
+
 DROP TABLE IF EXISTS `backward`;
-DROP TABLE IF EXISTS `forward`;
-DROP TABLE IF EXISTS `words`;
-
-CREATE TABLE `words` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `word` tinyblob,
-    `symbol` int(10) unsigned NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `no_dupe_symbols` (`symbol`),
-    UNIQUE KEY `no_dupe_words` (`word`(10))
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-LOCK TABLES `words` WRITE;
-INSERT INTO `words` VALUES (1,'<ERROR>',0),(2,'<FIN>',1);
-UNLOCK TABLES;
-
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `backward` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `symbol` int(10) unsigned NOT NULL,
-    `usage` int(10) unsigned NOT NULL DEFAULT '0',
-    `count` int(10) unsigned NOT NULL DEFAULT '0',
-    `parent` int(10) unsigned DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `no_dupe_symbols` (`symbol`,`parent`),
-    KEY `tree` (`id`,`parent`),
-    CONSTRAINT `bkeys` FOREIGN KEY (`symbol`) REFERENCES `words` (`symbol`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-LOCK TABLES `backward` WRITE;
-INSERT INTO `backward` VALUES (1,0,0,0,NULL);
-UNLOCK TABLES;
-
-CREATE TABLE `forward` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `symbol` int(10) unsigned NOT NULL,
+  `word` int(10) unsigned NOT NULL,
   `usage` int(10) unsigned NOT NULL DEFAULT '0',
   `count` int(10) unsigned NOT NULL DEFAULT '0',
   `parent` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `no_dupe_symbols` (`symbol`,`parent`),
+  UNIQUE KEY `word` (`word`,`parent`),
   KEY `tree` (`id`,`parent`),
-  CONSTRAINT `fkeys` FOREIGN KEY (`symbol`) REFERENCES `words` (`symbol`)
+  KEY `word_2` (`word`),
+  KEY `parent` (`parent`),
+  CONSTRAINT `backward_ibfk_1` FOREIGN KEY (`word`) REFERENCES `words` (`id`),
+  CONSTRAINT `backward_ibfk_2` FOREIGN KEY (`parent`) REFERENCES `backward` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `backward`
+--
+
+LOCK TABLES `backward` WRITE;
+/*!40000 ALTER TABLE `backward` DISABLE KEYS */;
+INSERT INTO `backward` VALUES (1,1,0,0,NULL);
+/*!40000 ALTER TABLE `backward` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forward`
+--
+
+DROP TABLE IF EXISTS `forward`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `forward` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `word` int(10) unsigned NOT NULL,
+  `usage` int(10) unsigned NOT NULL DEFAULT '0',
+  `count` int(10) unsigned NOT NULL DEFAULT '0',
+  `parent` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `word` (`word`,`parent`),
+  KEY `tree` (`id`,`parent`),
+  KEY `word_2` (`word`),
+  KEY `parent` (`parent`),
+  CONSTRAINT `forward_ibfk_1` FOREIGN KEY (`word`) REFERENCES `words` (`id`),
+  CONSTRAINT `forward_ibfk_2` FOREIGN KEY (`parent`) REFERENCES `forward` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `forward`
+--
 
 LOCK TABLES `forward` WRITE;
-INSERT INTO `forward` VALUES (1,0,0,0,NULL);
+/*!40000 ALTER TABLE `forward` DISABLE KEYS */;
+INSERT INTO `forward` VALUES (1,1,0,0,NULL);
+/*!40000 ALTER TABLE `forward` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `words`
+--
+
+DROP TABLE IF EXISTS `words`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `words` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `word` tinyblob,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `word` (`word`(10)),
+  KEY `id` (`id`,`word`(10))
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `words`
+--
+
+LOCK TABLES `words` WRITE;
+/*!40000 ALTER TABLE `words` DISABLE KEYS */;
+INSERT INTO `words` VALUES (1,'<ERROR>'),(2,'<FIN>');
+/*!40000 ALTER TABLE `words` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2008-11-16  8:47:32
--- include/halpy/sqltree.py
+++ include/halpy/sqltree.py
@@ -5,24 +5,40 @@ import warnings
 warnings.simplefilter("ignore")
 import MySQLdb
 
-class Tree(object):
+class SQLConnection(object):
 
-    def __init__(self, **kwargs):
-        self.connection = MySQLdb.connect(**kwargs)
+    def __init__(self, **dbinfo):
+        self.connection = MySQLdb.connect(**dbinfo)
 
-    def reset(self):
-        cursor = self.connection.cursor()
+    def __enter__(self):
+        return self.connection.cursor()
 
-    def __del__(self):
-        self.connection.close()
+    def __exit__(self, *args):
+        self.connection.commit()
+
+
+class SQLBase(object):
+
+    def __init__(self, table, connection):
+        self.table = table
+        self.connection = connection
+
+
+class SQLTree(SQLBase):
+
+    pass
+
+
+class SQLDictionary(SQLBase):
+
+    pass
 
 
 def main():
-    connection = dict(user='tree', passwd='tree', db='tree')
-    forward = Tree('forward', **connection)
-    forward.reset()
-    backward = Tree('backward', **connection)
-    backward.reset()
+    connection = SQLConnection(user='tree', passwd='tree', db='tree')
+    forward = SQLTree('forward', connection)
+    backward = SQLTree('backward', connection)
+    words = SQLDictionary('words', connection)
     return 0
 
 if __name__ == '__main__':
