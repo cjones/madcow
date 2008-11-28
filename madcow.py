7feeaee4f924e266187cf27a41a2bde5c61c4ff3
Chris Jones <cjones@gmail.com>
2008-11-28 05:53:22 +0000

    hi

--- madcow.py
+++ madcow.py
@@ -57,7 +57,7 @@ __all__ = [u'Madcow']
 MADCOW_URL = u'http://code.google.com/p/madcow/'
 CHARSET = u'utf-8'
 CONFIG = u'madcow.ini'
-SAMPLE_HASH = u'9581e50888d178b06b0afea402aabfe9'
+SAMPLE_HASH = u'e05f709da83edb8f02a3d6a48b1571e2'
 LOG = dict(level=log.WARN, stream=sys.stderr, datefmt=u'%x %X',
            format=u'[%(asctime)s] %(levelname)s: %(message)s')
 
