b003a79568646ea780bcc80936cb19378299a2ff
Chris Jones <cjones@gmail.com>
2009-02-10 07:15:10 +0000

    holy shit, memebot: so broke.

--- modules/memebot.py
+++ modules/memebot.py
@@ -30,270 +30,287 @@ from include.utils import Module
 import logging as log
 from sqlobject import *
 
-try:
-    class url(SQLObject):
-        url = StringCol()
-        clean = StringCol()
-        author = ForeignKey(u'author')
-        channel = ForeignKey(u'channel')
-        citations = IntCol(default=0)
-        posted = DateTimeCol(default = datetime.datetime.now)
-        comments = MultipleJoin(u'comments')
+class URL(SQLObject):
 
-        def truncated_url(self):
-            if (len(self.url) > 48):
-                return self.url[:48] + u' ... ' + self.url[-4:]
-            else:
-                return self.url
+    class sqlmeta:
 
-        turl = property(truncated_url)
+        table = 'url'
 
+    url = StringCol()
+    clean = StringCol()
+    author = ForeignKey('Author')
+    channel = ForeignKey('Channel')
+    citations = IntCol(default=0)
+    posted = DateTimeCol(default = datetime.datetime.now)
+    comments = MultipleJoin('Comments')
 
-    class author(SQLObject):
-        name = StringCol(alternateID=True, length=50)
-        urls = MultipleJoin(u'url')
-        comments = MultipleJoin(u'comments')
-        pointsNew = IntCol(default=0)
-        pointsOld = IntCol(default=0)
-        pointsCredit = IntCol(default=0)
+    @property
+    def truncated_url(self):
+        if len(self.url) > 48:
+            return self.url[:48] + ' ... ' + self.url[-4:]
+        else:
+            return self.url
 
 
-    class channel(SQLObject):
-        name = StringCol(alternateID=True, length=50)
-        urls = MultipleJoin(u'url')
+class Author(SQLObject):
+
+    name = StringCol(alternateID=True, length=50)
+    urls = MultipleJoin('URL')
+    comments = MultipleJoin('Comments')
+    points_new = IntCol(default=0)
+    points_old = IntCol(default=0)
+    points_credit = IntCol(default=0)
 
 
-    class comments(SQLObject):
-        text = StringCol()
-        author = ForeignKey(u'author')
-        url = ForeignKey(u'url')
-except:
-    pass
+class Channel(SQLObject):
+
+    name = StringCol(alternateID=True, length=50)
+    urls = MultipleJoin('URL')
 
 
-class Main(Module):
+class Comments(SQLObject):
+
+    text = StringCol()
+    author = ForeignKey('Author')
+    url = ForeignKey('URL')
+
+
+class MemeBot(Module):
+
     pattern = Module._any
     allow_threading = False
     priority = 10
     terminate = False
     require_addressing = False
-    help = u'score [name,range] - get memescore, empty for top10'
-    matchURL = re.compile(u'(http://\S+)', re.I)
-    scoreRequest = re.compile(r'^\s*score(?:(?:\s+|[:-]+\s*)(\S+?)(?:\s*-\s*('
-                              r'\S+))?)?\s*$', re.I)
-    colonHeader = re.compile(r'^\s*(.*?)\s*:\s*$')
-    get_frag = re.compile(r'^(.*)#([^;/?:@=&]*)$')
-    riffs = [u'OLD MEME ALERT!',
-             u'omg, SO OLD!',
-             u'Welcome to yesterday.',
-             u'been there, done that.',
-             u'you missed the mememobile.',
-             u'oldest. meme. EVAR.',
-             u'jesus christ you suck.',
-             u'you need a new memesource, bucko.',
-             u'that was funny the first time i saw it.',
-             u'new to the internet?',
-             u'i think that came installed with the internet']
+    help = 'score [name | x - y] - get memescore'
+    match_url_re = re.compile(r'(http://\S+)', re.I)
+    score_request_re = re.compile(
+            r'^\s*score(?:(?:\s+|[:-]+\s*)(\S+?)(?:\s*-\s*(\S+))?)?\s*$', re.I)
+    colon_header_re = re.compile(r'^\s*(.*?)\s*:\s*$')
+    get_frag_re = re.compile(r'^(.*)#([^;/?:@=&]*)$')
+    riffs = ['OLD MEME ALERT!',
+             'omg, SO OLD!',
+             'Welcome to yesterday.',
+             'been there, done that.',
+             'you missed the mememobile.',
+             'oldest. meme. EVAR.',
+             'jesus christ you suck.',
+             'you need a new memesource, bucko.',
+             'that was funny the first time i saw it.',
+             'new to the internet?',
+             'i think that came installed with the internet.',
+             'are you serious?']
 
     def __init__(self, madcow):
+        self.encoding = madcow.config.main.charset
         self.throttle = Throttle()
         config = madcow.config.memebot
         engine = config.db_engine
-        uri = engine + u'://'
-        if engine == u'sqlite':
+        uri = engine + '://'
+        if engine == 'sqlite':
             uri += os.path.join(madcow.prefix,
-                                u'data/db-%s-memes' % madcow.namespace)
-        elif engine == u'mysql':
+                                'data/db-%s-memes' % madcow.namespace)
+        else:
             user = config.db_user
-            if len(config.db_pass):
-                user += u':' + config.db_pass
+            if config.db_pass:
+                user += ':' + config.db_pass
             host = config.db_host
-            if not len(host):
-                host = u'localhost'
-            if len(config.db_port):
-                host += u':' + config.db_port
-            uri += u'%s@%s/%s' % (user, host, config.db_name)
+            if not host:
+                host = 'localhost'
+            if config.db_port:
+                host += ':' + config.db_port
+            uri += '%s@%s/%s' % (user, host, config.db_name)
         try:
+            log.warn('uri=%s' % uri)
             sqlhub.processConnection = connectionForURI(uri)
         except Exception, error:
-            log.warn(u'invalid uri: %s (%s)' % (uri, error))
+            log.warn('invalid uri: %s (%s)' % (uri, error))
             self.enabled = False
             return
 
         # show raw SQL being dispatched if loglevel is debug
-        if log.root.level <= log.DEBUG:
-            url._connection.debug = True
-            author._connection.debug = True
-            channel._connection.debug = True
-            comments._connection.debug = True
+        if True: #log.root.level <= log.DEBUG:
+            URL._connection.debug = True
+            Author._connection.debug = True
+            Channel._connection.debug = True
+            Comments._connection.debug = True
 
-        url.createTable(ifNotExists=True)
-        author.createTable(ifNotExists=True)
-        channel.createTable(ifNotExists=True)
-        comments.createTable(ifNotExists=True)
+        URL.createTable(ifNotExists=True)
+        Author.createTable(ifNotExists=True)
+        Channel.createTable(ifNotExists=True)
+        Comments.createTable(ifNotExists=True)
 
-    def cleanURL(self, url):
+    def clean_url(self, url):
         # stolen from urlparse.urlsplit(), which doesn't handle
         # splitting frags correctly
-        netloc = query = fragment = u''
-        i = url.find(u':')
+        netloc = query = fragment = ''
+        i = url.find(':')
         scheme = url[:i].lower()
         url = url[i+1:]
-        if url[:2] == u'//':
+        if url[:2] == '//':
             delim = len(url)
-            for c in u'/?#':
+            for c in '/?#':
                 wdelim = url.find(c, 2)
                 if wdelim >= 0:
                     delim = min(delim, wdelim)
             netloc, url = url[2:delim], url[delim:]
-        if u'#' in url:
+        if '#' in url:
             try:
-                url, fragment = self.get_frag.search(url).groups()
+                url, fragment = self.get_frag_re.search(url).groups()
             except:
                 pass
-        if u'?' in url:
-            url, query = url.split(u'?', 1)
+        if '?' in url:
+            url, query = url.split('?', 1)
 
         ### now for memebots normalizing..
         # make hostname lowercase and remove www
         netloc = netloc.lower()
-        if netloc.startswith(u'www.') and len(netloc) > 4:
+        if netloc.startswith('www.') and len(netloc) > 4:
             netloc = netloc[4:]
         # all urls have trailing slash
-        if url == u'':
-            url = u'/'
+        if url == '':
+            url = '/'
         # remove empty query settings, these are usually form artifacts
         # and put them in order
         try:
-            query = u'&'.join(sorted(
-                    u'='.join(i)
-                    for i in [p.split(u'=', 1) for p in query.split(u'&')]
-                    if len(i) == 2 and i[1]))
+            query = '&'.join(
+                    sorted('='.join(i)
+                           for i in [p.split('=', 1) for p in query.split('&')]
+                           if len(i) == 2 and i[1]))
         except:
-            query = u''
+            query = ''
         # ignore fragments
-        fragment = u''
+        fragment = ''
 
         return urlparse.urlunsplit([scheme, netloc, url, query, fragment])
 
-    def getScoreForAuthor(self, a):
-        return a.pointsNew + (a.pointsOld  * -2) + (a.pointsCredit * 2)
+    def get_score_for_author(self, author):
+        return ((author.points_new    *  1) +
+                (author.points_old    * -2) +
+                (author.points_credit *  2))
 
-    def getScores(self):
-        scores = [(a.name, self.getScoreForAuthor(a)) for a in author.select()]
-        scores = sorted(scores, lambda x, y: cmp(y[1], x[1]))
-        return scores
+    def get_scores(self):
+        scores = [(author.name, self.get_score_for_author(author))
+                   for author in Author.select()]
+        return sorted(scores, key=lambda item: item[1], reverse=True)
 
     def response(self, nick, args, kwargs):
         nick = nick.lower()
-        chan = kwargs[u'channel'].lower()
-        addressed = kwargs[u'addressed']
-        message = args[0]
+        addressed = kwargs['addressed']
+        message = args[0].encode(self.encoding, 'replace')
 
         if addressed:
             try:
-                x, y = self.scoreRequest.search(message).groups()
-                scores = self.getScores()
-                size = len(scores)
-
-                if x is None:
-                    scores = scores[:10]
-                    x = 1
-                elif x.isdigit():
-                    x = int(x)
-                    if x == 0:
-                        x = 1
-                    if x > size:
-                        x = size
-
-                    if y is not None and y.isdigit():
-                        y = int(y)
-                        if y > size:
-                            y = size
-                        scores = scores[x-1:y]
-                    else:
-                        scores = [scores[x-1]]
-
-                else:
-                    for i, data in enumerate(scores):
-                        name, score = data
-                        if name.lower() == x.lower():
-                            scores = [scores[i]]
-                            x = i+1
-                            break
-
-                out = []
-                for i, data in enumerate(scores):
-                    name, score = data
-                    out.append(u'#%s: %s (%s)' % (i + x, name, score))
-                return u', '.join(out)
-
-            except:
+                x, y = self.score_request_re.search(message).groups()
+                return self.score_response(x, y)
+            except AttributeError:
                 pass
 
-        match = self.matchURL.search(message)
-        if match is None:
+        try:
+            orig = self.match_url_re.search(message).group(1)
+        except AttributeError:
             return
 
-        event = self.throttle.registerEvent(name=u'memebot', user=nick)
+        event = self.throttle.registerEvent(name='memebot', user=nick)
         if event.isThrottled():
             if event.warn():
-                return u'%s: Stop abusing me plz.' % nick
+                return '%s: Stop abusing me plz.' % nick
             else:
                 return
 
-        orig = match.group(1)
-        clean = self.cleanURL(orig)
+        clean = self.clean_url(orig)
 
         comment1, comment2 = re.split(re.escape(orig), message)
         try:
-            comment1 = self.colonHeader.search(comment1).group(1)
-        except:
+            comment1 = self.colon_header_re.search(comment1).group(1)
+        except AttributeError:
             pass
 
         comment1 = comment1.strip()
         comment2 = comment2.strip()
 
         try:
-            me = author.byName(nick)
+            me = Author.byName(nick)
         except SQLObjectNotFound:
-            me = author(name=nick)
+            me = Author(name=nick)
 
         try:
-            # old meme
             try:
-                old = url.select(url.q.clean == clean)[0]
+                old = URL.select(URL.q.clean == clean)[0]
             except:
                 raise SQLObjectNotFound
 
-            if len(comment1) > 0:
+            if comment1:
                 comments(url=old, text=comment1, author=me)
-            if len(comment2) > 0:
+            if comment2:
                 comments(url=old, text=comment2, author=me)
 
             # chew them out unless its my own
-            if old.author.name.lower() != nick.lower():
-                response = u'first posted by %s on %s' % (old.author.name,
-                                                         old.posted)
-                riff = random.choice(self.riffs)
-                old.author.pointsCredit = old.author.pointsCredit + 1
-                me.pointsOld = me.pointsOld + 1
+            if old.author.name.lower() != nick:
+                old.author.points_credit = old.author.points_credit + 1
+                me.points_old = me.points_old + 1
                 old.citations = old.citations + 1
-                return u'%s %s' % (riff, response)
+
+                return '%s First posted by %s on %s' % (
+                        random.choice(self.riffs),
+                        old.author.name,
+                        old.posted)
 
         except SQLObjectNotFound:
+            channel = kwargs['channel'].lower()
             try:
-                c = channel.byName(chan)
+                channel = Channel.byName(channel)
             except SQLObjectNotFound:
-                c = channel(name=chan)
-            urlid = url(url=orig, clean=clean, author=me, channel=c)
-            if len(comment1) > 0:
-                comments(url=urlid, text=comment1, author=me)
-            if len(comment2) > 0:
-                comments(url=urlid, text=comment2, author=me)
-            me.pointsNew = me.pointsNew + 1
+                channel = Channel(name=channel)
+
+            url = URL(url=orig, clean=clean, author=me, channel=channel)
+            print '*** new ID: %d' % url.id
+
+            if comment1:
+                comments(url=url, text=comment1, author=me)
+            if comment2:
+                comments(url=url, text=comment2, author=me)
+            me.points_new = me.points_new + 1
 
         except Exception, error:
-            log.warn(u'error in module %s' % self.__module__)
+            log.warn('error in module %s' % self.__module__)
             log.exception(error)
+
+    def score_response(self, x, y):
+        scores = self.get_scores()
+        size = len(scores)
+
+        if x is None:
+            scores = scores[:10]
+            x = 1
+        elif x.isdigit():
+            x = int(x)
+            if x == 0:
+                x = 1
+            if x > size:
+                x = size
+
+            if y and y.isdigit():
+                y = int(y)
+                if y > size:
+                    y = size
+                scores = scores[x-1:y]
+            else:
+                scores = [scores[x-1]]
+        else:
+            for i, data in enumerate(scores):
+                name, score = data
+                if name.lower() == x.lower():
+                    scores = [scores[i]]
+                    x = i+1
+                    break
+
+        out = []
+        for i, data in enumerate(scores):
+            name, score = data
+            out.append('#%s: %s (%s)' % (i + x, name, score))
+        return ', '.join(out)
+
+
+Main = MemeBot
 
