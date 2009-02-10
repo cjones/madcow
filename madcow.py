867c30fde141a0b58b0f1c647b2c3d429cbfa138
Chris Jones <cjones@gmail.com>
2009-02-10 22:50:18 +0000

    add world clock to config

--- madcow.py
+++ madcow.py
@@ -57,7 +57,7 @@ __all__ = [u'Madcow']
 MADCOW_URL = u'http://code.google.com/p/madcow/'
 CHARSET = u'utf-8'
 CONFIG = u'madcow.ini'
-SAMPLE_HASH = u'94b33a2875196aa761733f45b3cd55c0'
+SAMPLE_HASH = u'3e45185d0bd709dbace347971ff05f88'
 LOG = dict(level=log.WARN, stream=sys.stderr, datefmt=u'%x %X',
            format=u'[%(asctime)s] %(levelname)s: %(message)s')
 
