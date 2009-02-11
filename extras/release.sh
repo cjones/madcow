24a777da86adf3f3041d30a2615c5025184f3c7c
Chris Jones <cjones@gmail.com>
2009-02-11 19:57:14 +0000

    prep 1.5.4

--- extras/release.sh
+++ extras/release.sh
@@ -2,7 +2,7 @@
 # diff trunk from previous release and update ChangeLog
 # make a release tag:
 
-release="madcow-1.5.3"
+release="madcow-1.5.4"
 
 svn copy https://madcow.googlecode.com/svn/trunk https://madcow.googlecode.com/svn/tags/$release || exit
 
