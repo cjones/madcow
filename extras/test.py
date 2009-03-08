fd2a9fb534d81528212a8c3b029d558bb3c56c7e
Chris Jones <cjones@gmail.com>
2009-03-08 04:29:04 +0000

    update test suite

--- extras/test.py
+++ extras/test.py
@@ -8,176 +8,119 @@ import sys
 import re
 
 any = re.compile(r'.+')
-tests = {
-        u'learn': {
-            u'request': u'set karma cj_ 31337',
-            u'result': u"test: set cj_'s karma to 31337",
-            },
-        u'wikiquotes': {
-            u'request': u'wq hitler',
-            u'result': any,
-            },
-        u'hugs': {
-            u'request': u'hugs',
-            u'result': any,
-            },
-        u'grufti': {
-            u'request': u'penis',
-            u'result': re.compile(
-                r'hi\. :\)|8===D ~ ~|it that way\.|Joel\.  :\('),
-            },
-        u'google': {
-            u'request': u'google google',
-            u'result': u'test: google = http://www.google.com/',
-            },
-        u'lyrics': {
-            u'request': u'sing 1979',
-            u'result': re.compile(r'Shakedown 1979'),
-            },
-        u'bbcnews': {
-            u'request': u'bbcnews',
-            u'result': re.compile(r'http://news.bbc.co.uk/'),
-            },
-        u'weather': {
-            u'request': u'fc 94005',
-            u'result': re.compile(r'Brisbane, California'),
-            },
-        u'seen': {
-            u'request': u'seen test',
-            u'result': re.compile(
-                u'test: test was last seen 0 seconds ago on test'),
-            },
-        u'memebot': {
-            u'request': u'http://google.com/',
-            u'result': re.compile(r'first posted by j0no'),
-            },
-        u'woot': {
-            u'request': u'woot',
-            u'result': re.compile(r'http://www.woot.com/'),
-            },
-        u'area': {
-            u'request': u'area 707',
-            u'result': u'test: 707 = Santa Rosa, California',
-            },
-        u'webtender': {
-            u'request': u'drinks fuzzy navel',
-            u'result': re.compile(r'test: Fuzzy Navel - 1/3 Peach schnapps'),
-            },
-        u'wikipedia': {
-                u'request': u'wiki wikipedia',
-                u'result': re.compile(
-                    r'Wikipedia - Wikipedia is a free, multilingual'),
-                },
-        u'chp': {
-                u'request': u'chp 101',
-                u'result': re.compile(r'(No incidents found|=>\s+[0-9:]+[AP]M)'),
-                },
-        u'factoids': [
-                {
-                    u'request': u'foo is bar',
-                    u'result': u'OK, test',
-                    },
-                {
-                    u'request': u'foo?',
-                    u'result': re.compile(r'foo.*?(is|was).*?bar'),
-                    },
-                {
-                    u'request': u'forget foo',
-                    u'result': u'test: I forgot foo',
-                    },
-                ],
-        u'calc': {
-                u'request': u'calc 1+1',
-                u'result': u'test: 1 + 1 = 2',
-                },
-        u'war': {
-                u'request': u'terror',
-                u'result': re.compile(
-                    u'Terror: \x1b\[1;33m.*?\x1b\[0m, DoomsDay: It is \d+ Minu'
-                    u'tes to Midnight, IranWar: .*?, IraqWar: .*?, BodyCount'),
-                },
-        u'urban': {
-                u'request': u'urban penis',
-                u'result': re.compile(r'test: \[1/20\] The tool used to wean'),
-                },
-        u'dictionary': {
-                u'request': u'define penis',
-                u'result': re.compile(r'a male erectile organ'),
-                },
-        u'conservapedia': {
-                u'request': u'cp penis',
-                u'result': re.compile(
-                    r'Human reproduction - Human reproduction'),
-                },
-        u'slut': {
-                u'request': u'slutcheck penis',
-                u'result': re.compile(r'penis is [0-9.]+% slutty'),
-                },
-        u'bash': [
-                {
-                    u'request': u'bash cj_',
-                    u'result': re.compile(r'cj'),
-                    },
-                {
-                    u'request': u'qdb cj_',
-                    u'result': re.compile(r'cj'),
-                    },
-                {
-                    u'request': u'limerick',
-                    u'result': any,
-                    },
-                ],
-        u'nslookup': {
-                u'request': u'nslookup localhost',
-                u'result': u'test: 127.0.0.1',
-                },
-        u'bible': {
-                u'request': u'bible john 3:16',
-                u'result': re.compile(
-                    u'"For God so loved the world that he gave'),
-                },
-        u'yourmom': {
-                u'request': u'yourmom',
-                u'result': any,
-                },
-        u'roll': {
-                u'request': u'roll 2d20',
-                u'result': re.compile(r'test rolls \d+, needs \d+, test'),
-                },
-        u'livejournal': {
-                u'request': u'lj cj_',
-                u'result': re.compile(
-                    r'HI PEOPLE WHO HAVE ME FRIENDED FOR SOME REASON'),
-                },
-        u'summon': {
-                u'request': u'summon asdf',
-                u'result': re.compile(r"I don't know the email for asdf"),
-                },
-        u'karma': {
-                u'request': u'karma cj_',
-                u'result': re.compile(r"test: cj_'s karma is \d+"),
-                },
-        u'babel': {
-                u'request': u'translate from english to spanish: your mom',
-                u'result': u'test: tu madre',
-                },
-        u'movie': {
-                u'request': u'rate bone collector',
-                u'result': (
-                    u'test: IMDB: 6.3/10, Freshness: 28%, Meta - Critics: 45/1'
-                    u'00, Users: 5.0/10'),
-                },
-        u'stockquote': {
-                u'request': u'quote goog',
-                u'result': re.compile(r'Google Inc\.  \(GOOG\)'),
-                },
-        u'artfart': {
-                u'request': u'artfart',
-                u'result': re.compile(r'>>> .+ <<<'),
-                },
-
-        u'delicious': None,
-    u'jinx': None,
+tests = {'learn': {'request': 'set karma cj_ 31337',
+                   'result': "test: set cj_'s karma to 31337"},
+         'wikiquotes': {'request': 'wq hitler', 'result': any},
+         'hugs': {'request': 'hugs', 'result': any},
+         'grufti': {'request': 'penis',
+                    'result': re.compile(
+                        r'hi\. :\)|8===D ~ ~|it that way\.|Joel\.  :\(|DOING'
+                        r' DOING')},
+         'google': {'request': 'google google',
+                    'result': 'test: google = http://www.google.com/'},
+         'lyrics': {'request': 'sing 1979',
+                    'result': re.compile(r'Shakedown 1979')},
+         'bbcnews': {'request': 'bbcnews',
+                     'result': re.compile(r'http://news.bbc.co.uk/')},
+         'weather': {'request': 'fc 94005',
+                     'result': re.compile(r'Brisbane, California')},
+         'seen': {'request': 'seen test',
+                  'result': re.compile(
+                      'test: test was last seen 0 seconds ago on test')},
+         'memebot': {'request': 'http://google.com/',
+                     'result': re.compile(r'First posted by j0no')},
+         'woot': {'request': 'woot',
+                  'result': re.compile(r'http://www.woot.com/')},
+         'area': {'request': 'area 707',
+                  'result': 'test: 707 = Santa Rosa, California'},
+         'webtender': {'request': 'drinks fuzzy navel',
+                       'result': re.compile(
+                           r'test: Fuzzy Navel - 1/3 Peach schnapps')},
+         'wikipedia': {'request': 'wiki wikipedia',
+                       'result': re.compile(
+                           r'Wikipedia - Wikipedia is a free, multilingual')},
+         'chp': {'request': 'chp 101',
+                 'result': re.compile(
+                     r'(No incidents found|=>\s+[0-9:]+[AP]M)')},
+         'factoids': [{'request': 'foo is bar',
+                       'result': 'OK, test'},
+                      {'request': 'foo?',
+                       'result': re.compile(r'foo.*?(is|was).*?bar')},
+                      {'request': 'forget foo',
+                       'result': 'test: I forgot foo'}],
+         'calc': {'request': 'calc 1+1', 'result': 'test: 1 + 1 = 2'},
+         'war': {'request': 'terror',
+                 'result': re.compile(
+                     'Terror: \x1b\[1;33m.*?\x1b\[0m, DoomsDay: It is \d+ Minu'
+                     'tes to Midnight, IranWar: .*?, IraqWar: .*?, BodyCount')},
+         'urban': {'request': 'urban penis',
+                   'result': re.compile(
+                       r'test: \[1/20\] The tool used to wean')},
+         'dictionary': {'request': 'define penis',
+                        'result': re.compile(r'a male erectile organ')},
+         'conservapedia': {'request': 'cp penis',
+                           'result': re.compile(
+                               r'Human reproduction - Human reproduction')},
+         'slut': {'request': 'slutcheck penis',
+                  'result': re.compile(r'penis is [0-9.]+% slutty')},
+         'bash': [{'request': 'bash cj_', 'result': re.compile(r'cj', re.I)},
+                  {'request': 'qdb cj_', 'result': re.compile(r'cj', re.I)},
+                  {'request': 'limerick', 'result': any}],
+         'nslookup': {'request': 'nslookup localhost',
+                      'result': 'test: 127.0.0.1'},
+         'bible': {'request': 'bible john 3:16',
+                   'result': re.compile(
+                       '"For God so loved the world that he gave')},
+         'yourmom': {'request': 'yourmom', 'result': any},
+         'roll': {'request': 'roll 2d20',
+                  'result': re.compile(r'test rolls \d+, needs \d+, test')},
+         'livejournal': {'request': 'lj cj_',
+                         'result': re.compile(r'THIS IS MY UPDATE')},
+         'summon': {'request': 'summon asdf',
+                    'result': re.compile(r"I don't know the email for asdf")},
+         'karma': {'request': 'karma cj_',
+                   'result': re.compile(r"test: cj_'s karma is \d+")},
+         'babel': {'request': 'translate from english to spanish: your mom',
+                   'result': 'test: tu madre'},
+         'movie': {'request': 'rate bone collector',
+                   'result': (
+                       'test: IMDB: 6.3/10, Freshness: 28%, Meta - Critics: 4'
+                       '5/100, Users: 5.0/10')},
+         'stockquote': {'request': 'quote goog',
+                        'result': re.compile(r'Google Inc\.')},
+         'artfart': {'request': 'artfart',
+                     'result': re.compile(r'>>> .+ <<<')},
+         'alias': [{'request': 'alias add g quote goog',
+                    'result': 'alias added'},
+                   {'request': 'alias del 1', 'result': 'deleted alias: g'}],
+         'care': {'request': 'care 50',
+                  'result': (
+                      'CARE-O-METER: |....................o..................'
+                      '.|')},
+         'clock': {'request': 'time in budapest',
+                   'result': re.compile(
+                       r'test:\s+\d+:\d+[ap]m\s+\w+\s+\(CET\)\s+-\s+Time in B'
+                       r'udapest, Hungary')},
+         'obama': {'request': 'obama',
+                   'result': re.compile(r'Bush has been gone.*?\d')},
+         'election2008': {'request': 'ev',
+                          'result': re.compile(
+                              r'^test: Projected Senate Seats 2010:.*?Democra'
+                              r'ts.*?: \d+, .*?Republicans.*?: \d+')},
+         'spellcheck': {'request': 'spell untied',
+                        'result': 'test: united'},
+         'figlet': {'request': 'figlet hi', 'result': any},
+         'fmylife': {'request': 'fml', 'result': any},
+         'joke': {'request': 'joke', 'result': any},
+         'megahal': {'request': 'mh hi',
+                     'result': 'I am utterly speechless!'},
+         'traffic': {'request': 'traffic from sunnyvale to brisbane',
+                     'result': re.compile(r'test: \d+ mins\.')},
+         'wardb': {'request': 'wardb rock',
+                   'result': re.compile(r'.*?Azure-rock.*?: No bonuses')},
+         'delicious': None,
+         'jinx': None,
+         'steam': None,
 }
 
 retype = type(re.compile(u''))
