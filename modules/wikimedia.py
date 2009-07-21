4ce5d4b097eab4876689cda0806d04f3ab84d064
Chris Jones <cjones@gmail.com>
2009-07-21 04:31:22 +0000

    rework wiki modules

---
+++ modules/wikimedia.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+
+"""Handles WikiMedia queries"""
+
+from BeautifulSoup import BeautifulSoup
+from include.useragent import geturl
+from include.utils import stripHTML
+from include.utils import Module
+from urlparse import urljoin
+import logging as log
+import re
+
+# wiki configuration
+WIKIS = {'wikipedia': {
+             'keys': ['wp', 'wiki', 'wikipedia'],
+             'baseurl': 'http://en.wikipedia.org/',
+             'kwargs': {
+                 'random': '/wiki/Special:Random',
+                 'search': '/wiki/Special:Search',
+                 'advert': ' - Wikipedia, the free encyclopedia',
+                 'error': 'Search results',
+                 },
+             },
+         'conservapedia': {
+             'keys': ['cp'],
+             'baseurl': 'http://www.conservapedia.com/',
+             'kwargs': {
+                 'random': '/Special:Random',
+                 'search': '/Special:Search',
+                 'advert': ' - Conservapedia',
+                 'error': 'Search results',
+                 },
+             },
+         'christopedia': {
+             'keys': ['ch'],
+             'baseurl': 'http://christopedia.us/',
+             'kwargs': {
+                 'random': '/Special:Random',
+                 'search': '/Special:Search',
+                 'advert': ' - Christopedia, the Christian encyclopedia',
+                 'error': 'Search results',
+                 },
+             },
+         'encyclopediadramatica': {
+             'keys': ['ed', 'drama'],
+             'baseurl': 'http://encyclopediadramatica.com/',
+             'kwargs': {
+                 'random': '/Special:Random',
+                 'search': '/Special:Search',
+                 'advert': ' - Encyclopedia Dramatica',
+                 'error': 'Search results',
+                 },
+             },
+         }
+
+class WikiMedia(object):
+
+    citations_re = re.compile(r'\[.*?\]', re.DOTALL)
+    parens_re = re.compile(r'\(.*?\)', re.DOTALL)
+    whitespace_re = re.compile(r'[ \t\r\n]+')
+    fix_punc_re = re.compile(r'\s+([,;:.])')
+    sentence_re = re.compile(r'(.*?\.)\s+', re.DOTALL)
+    summary_size = 400
+    scripts_re = re.compile(r'<script [^>]+>.*?</script>', re.I | re.DOTALL)
+
+    def __init__(self, baseurl, **kwargs):
+        self.baseurl = baseurl
+        self.__dict__.update(kwargs)
+
+    def getsummary(self, query):
+        if not self.search:
+            return u"i don't know how to search this wiki!"
+        opts = {'search': query, 'go': 'Go'}
+        return self._getsummary(self.search_url, opts=opts)
+
+    def getrandom(self):
+        if not self.random:
+            return u"i don't know where random pages are on this wiki!"
+        return self._getsummary(self.random_url)
+
+    def _getsummary(self, url, opts=None):
+        soup, title = self._getpage(url, opts)
+
+        # massage into plain text by concatenating paragraphs
+        content = ' '.join(p.renderContents().decode('utf-8')
+                           for p in soup.findAll('p'))
+
+        # clean up rendered text
+        content = stripHTML(content)                    # strip markup
+        content = self.citations_re.sub(u'', content)   # remove citations
+        content = self.parens_re.sub(u'', content)      # remove parentheticals
+        content = self.whitespace_re.sub(u' ', content) # compress whitespace
+        content = self.fix_punc_re.sub(r'\1', content)  # fix punctuation
+        content = content.strip()                       # strip whitespace
+
+        # generate summary by adding as many sentences as possible before limit
+        summary = u'%s -' % title
+        for sentence in self.sentence_re.findall(content):
+            if len(summary + sentence) >= self.summary_size:
+                break
+            summary += ' ' + sentence
+        return summary
+
+    def _getpage(self, url, opts=None):
+        page = geturl(url, referer=self.baseurl, opts=opts)
+        # HTMLParser doesn't handle this very well.. see:
+        # http://www.crummy.com/software/BeautifulSoup/3.1-problems.html
+        page = self.scripts_re.sub('', page)
+        soup = BeautifulSoup(page)
+
+        # get page title
+        title = soup.title.string
+        if self.advert and self.advert in title:
+            title = title.replace(self.advert, '')
+
+        # remove all tabular data/sidebars
+        for table in soup.findAll('table'):
+            table.extract()
+
+        # remove disambiguation links
+        for div in soup.findAll('div', 'dablink'):
+            div.extract()
+
+        # remove latitude/longitude metadata for places
+        for span in soup.findAll('span', id='coordinates'):
+            span.extract()
+
+        # strip non-english content wrappers
+        for span in soup.findAll('span', lang=True):
+            span.extract()
+
+        # remove IPA pronounciation guidelines
+        for span in soup.findAll('span', 'IPA'):
+            span.extract()
+        for a in soup.findAll('a', text='IPA'):
+            a.extract()
+        for span in soup.findAll('span', 'audiolink'):
+            span.extract()
+
+        return soup, title
+
+    @property
+    def search_url(self):
+        return urljoin(self.baseurl, self.search)
+
+    @property
+    def random_url(self):
+        return urljoin(self.baseurl, self.random)
+
+
+def make_help(wikis):
+    """Generate madcow help from wiki config"""
+    help = []
+    for wiki, opts in wikis.iteritems():
+        item = []
+        if len(opts['keys']) > 1:
+            item.append('<')
+        item.append('|'.join(opts['keys']))
+        if len(opts['keys']) > 1:
+            item.append('>')
+        if opts['kwargs']['search']:
+            item.append(' ')
+        if opts['kwargs']['random']:
+            item.append('[')
+        else:
+            item.append('<')
+        item.append('query')
+        if opts['kwargs']['random']:
+            item.append(']')
+        else:
+            item.append('>')
+        item.append(' - search ' + wiki)
+        help.append(''.join(item))
+    return '\n'.join(help)
+
+
+class Main(Module):
+
+    """Autoloaded by Madcow"""
+
+    pattern = Module._any
+    terminate = False
+    help = make_help(WIKIS)
+    match_fmt = r'^\s*(?:%s)(?:\s+(.+?))?\s*$'
+
+    def __init__(self, madcow=None, wikis=None):
+        self.madcow = madcow
+        if wikis is None:
+            wikis = WIKIS
+        self.wikis = {}
+        for wiki, opts in wikis.iteritems():
+            match_re = self.match_fmt % '|'.join(opts['keys'])
+            match_re = re.compile(match_re, re.I)
+            handler = WikiMedia(opts['baseurl'], **opts['kwargs'])
+            self.wikis[wiki] = {'match_re': match_re, 'handler': handler}
+
+    def response(self, nick, args, kwargs):
+        try:
+            message = args[0]
+            for wiki, opts in self.wikis.iteritems():
+                try:
+                    query = opts['match_re'].search(message).group(1)
+                    if query:
+                        response = opts['handler'].getsummary(query)
+                    else:
+                        response = opts['handler'].getrandom()
+                    if response:
+                        return u'%s: %s' % (nick, response)
+                except AttributeError:
+                    pass
+        except Exception, error:
+            log.warn(u'error in module %s' % self.__module__)
+            log.exception(error)
+
+if __name__ == '__main__':
+    from include.utils import test_module
+    test_module(Main)
--- modules/wikiquotes.py
+++ modules/wikiquotes.py
@@ -19,17 +19,141 @@
 
 """Plugin to return random quote from WikiQuotes"""
 
-from include.wiki import Wiki
-from include.utils import stripHTML, Module
-import re
-import random
+from urlparse import urljoin
 import logging as log
+import random
+import re
+
+from BeautifulSoup import BeautifulSoup
+
+from include.utils import stripHTML, Module
+from include.useragent import geturl
 
 _pattern = re.compile(r'^\s*(?:wikiquote|wq)\s*(?:\s+(.*?)\s*)?$', re.I)
 _linebreak = re.compile(r'[\r\n]+')
 _whitespace = re.compile(r'\s{2,}')
 _author = u'random'
 _max = 10
+
+class Wiki(object):
+
+    """Return summary from WikiMedia projects"""
+
+    # site-specific details, default is english wikipedia
+    base_url = u'http://en.wikipedia.org/'
+    random_path = u'/wiki/Special:Random'
+    search_path = u'/wiki/Special:Search'
+    advert = u' - Wikipedia, the free encyclopedia'
+    error = u'Search results'
+
+    # size of response
+    summary_size = 400
+    sample_size = 32 * 1024
+
+    # precompiled regex
+    _citations = re.compile(r'\[.*?\]', re.DOTALL)
+    _audio = re.compile(r'audiolink', re.I)
+    _parens = re.compile(r'\(.*?\)', re.DOTALL)
+    _whitespace = re.compile(r'[ \t\r\n]+')
+    _sentence = re.compile(r'(.*?\.)\s+', re.DOTALL)
+    _fix_punc = re.compile(r'\s+([,;:.])')
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def get_summary(self, query):
+        soup, title = self.get_soup(query)
+
+        # check if this is a disambiguation page, if so construct special page
+        # there isn't a consistent style guide, so we just try to do the
+        # most common format (ordered list of links). if this fails, return
+        # a friendly failure for now
+        if soup.find(u'div', attrs={u'id': u'disambig'}):
+            try:
+                summary = u'%s (Disambiguation) - ' % title
+                for link in soup.find(u'ul').findAll(u'a'):
+                    title = unicode(link[u'title']).strip()
+                    if len(summary) + len(title) + 2 > self.summary_size:
+                        break
+                    if not summary.endswith(u' '):
+                        summary += u', '
+                    summary += title
+            except:
+                summary = u'Fancy, unsupported disambiguation page!'
+            return summary
+
+        # massage into plain text by concatenating paragraphs
+        content = []
+        for para in soup.findAll(u'p'):
+            content.append(unicode(para))
+        content = u' '.join(content)
+
+        # clean up rendered text
+        content = stripHTML(content)                 # strip markup
+        content = Wiki._citations.sub(u'', content)   # remove citations
+        content = Wiki._parens.sub(u'', content)      # remove parentheticals
+        content = Wiki._whitespace.sub(u' ', content) # compress whitespace
+        content = Wiki._fix_punc.sub(r'\1', content) # fix punctuation
+        content = content.strip()                    # strip whitespace
+
+        # search error
+        if title == self.error:
+            return u'No results found for "%s"' % query
+
+        # generate summary by adding as many sentences as possible before limit
+        summary = u'%s -' % title
+        for sentence in Wiki._sentence.findall(content):
+            if len(summary) + 1 + len(sentence) > self.summary_size:
+                break
+            summary += u' %s' % sentence
+        return summary
+
+    def get_soup(self, query):
+        if isinstance(query, (list, tuple)):
+            query = u' '.join(query)
+
+        # load page
+        if query == u'random':
+            opts = {}
+            url = urljoin(self.base_url, self.random_path)
+        else:
+            opts = {u'search': query, u'go': u'Go'}
+            url = urljoin(self.base_url, self.search_path)
+        page = geturl(url, referer=self.base_url, opts=opts,
+                      size=self.sample_size)
+
+        # create BeautifulSoup document tree
+        soup = BeautifulSoup(page)
+
+        # extract title minus WP advert
+        title = soup.title.string.replace(self.advert, u'')
+
+        # remove all tabular data/sidebars
+        for table in soup.findAll(u'table'):
+            table.extract()
+
+        # remove disambiguation links
+        for dablink in soup.findAll(u'div', attrs={u'class': u'dablink'}):
+            dablink.extract()
+
+        # remove latitude/longitude metadata for places
+        for coord in soup.findAll(u'span', attrs={u'id': u'coordinates'}):
+            coord.extract()
+
+        # strip non-english content wrappers
+        for span in soup.findAll(u'span', attrs={u'lang': True}):
+            span.extract()
+
+        # remove IPA pronounciation guidelines
+        for span in soup.findAll(u'span', attrs={u'class': u'IPA'}):
+            span.extract()
+        for link in soup.findAll(u'a', text=u'IPA'):
+            link.extract()
+        for span in soup.findAll(u'span', attrs={u'class': Wiki._audio}):
+            span.extract()
+
+        return soup, title
+
 
 class WikiQuotes(Wiki):
 
