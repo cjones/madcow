12cfcdf93354d076395be313b8ad1909ffc6a445
Chris Jones <cjones@gmail.com>
2011-09-26 09:27:08 +0000

    argh

--- contrib/django-memebot/gruntle/manage.py
+++ contrib/django-memebot/gruntle/manage.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python
 
 import sys
-from django.core.management import execute_manager
+import os
 
 sys.dont_write_bytecode = True
 
+from django.core.management import execute_manager, setup_environ
 import settings
 
 def main():
-    if len(sys.argv) == 2 and sys.argv[1] == 'runserver':
-        server_addr = getattr(settings, 'DEV_SERVER_ADDR', None)
-        if server_addr:
-            sys.argv.append(server_addr)
+    args = sys.argv[1:]
+    nargs = len(args)
+    if nargs and args[0] == 'runserver':
+
+        # turn on debugging dynamically
+        setup_environ(settings)
+        from django.conf import settings as real_settings
+        real_settings.TEMPLATE_DEBUG = real_settings.DEBUG = True
+
+        # mark ourselvs as a dev server
+        os.environ['DEV_SERVER'] = 'true'
+
+        # if not specified, use the binding in settings
+        if nargs == 1:
+            server_addr = getattr(settings, 'DEV_SERVER_ADDR', None)
+            if server_addr:
+                sys.argv.append(server_addr)
+
     execute_manager(settings)
 
 if __name__ == '__main__':
--- contrib/django-memebot/gruntle/urls.py
+++ contrib/django-memebot/gruntle/urls.py
@@ -1,5 +1,28 @@
-from django.conf.urls.defaults import *
+"""Root URL config"""
 
+import os
+
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+def mkpat(path):
+    """Normalize path to expected pattern"""
+    path = os.path.join(os.path.normpath(path), '')
+    if path.startswith('/'):
+        path = path[1:]
+    if path:
+        path = '^' + path
+    return path
+
+isdev = os.environ.get('DEV_SERVER', 'false') == 'true'
+
+# include memebot's URLs to SITE_ROOT
 urlpatterns = patterns('',
-        (r'', include('gruntle.memebot.urls')),
+        (mkpat(settings.SITE_ROOT) if isdev else '', include('gruntle.memebot.urls')),
         )
+
+# if this is a server run by ./manage.py runserver, serve static media
+if isdev:
+    urlpatterns = patterns('',
+            url(mkpat(settings.MEDIA_URL) + '(?P<path>.*)', 'django.views.static.serve', {
+                'document_root': settings.MEDIA_ROOT, 'show_indexes': True}, name='static-media')) + urlpatterns
