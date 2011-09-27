bcb65f224858f87da2083d60035bf02724447a90
Chris Jones <cjones@gmail.com>
2011-09-27 08:13:13 +0000

    stub for HTML scanner

--- contrib/django-memebot/gruntle/memebot/scanner/html.py
+++ contrib/django-memebot/gruntle/memebot/scanner/html.py
@@ -1,50 +1,30 @@
+"""Extracts summary data from HTML pages if possible"""
+
+from gruntle.memebot.scanner import Scanner, InvalidContent, ScanResult
+from gruntle.memebot.utils import trapped, text
+
+class HTMLScanner(Scanner):
+
+    rss_template = 'memebot/scanner/rss/html.html'
+
+    def handle(self, response, log):
+        if response.data_type != 'soup':
+            raise InvalidContent(response, 'Not an HTML file')
+        soup = response.data
+
+        title = None
+        with trapped:
+            title = text.decode(soup.head.title.string).strip()
+            log.info('Title: %r', title)
 
 
-'''
-class ScanResult(collections.namedtuple('ScanResult', 'response override_url title content_type content')):
+        # XXX remove me when we make some progress :(
+        raise InvalidContent('not ready to use this yet')
+        return ScanResult(response=response,
+                          override_url=None,
+                          title=title,
+                          content_type=None,
+                          content=None)
 
 
-
-
-                    # a hotlinked image
-                    if response.data_type == 'image' and Image is not None:
-
-                    # html page.. extract some info
-                    elif response.data_type == 'soup':
-                        log.info('Content: HTML page')
-
-                        # get title of page
-                        with trapped:
-                            link.title = text.decode(response.data.title.string).strip()
-                            log.info('Title: %r', link.title)
-
-                    # no idea.. just save what we got
-                    else:
-                        log.info('Content: Unknown (%s)', response.content_type)
-                        link.content_type = response.content_type
-
-                else:
-                    log.info('Error: %d %s', response.code, response.msg)
-                    publish = False
-                    link.content = response.msg
-                    if response.code == 404:
-                        fatal = True
-
-        except TrapError, exc:
-            log.warn('Failure scanning link', exc_info=exc.args)
-            link.content = exc
-            publish = False
-
-        if publish:
-            link.publish(commit=False)
-        else:
-            link.error_count += 1
-            if fatal or (max_errors is not None and link.error_count >= max_errors):
-                link.state = 'invalid'
-                log.info('Link is marked as permanently invalid')
-
-        if dry_run:
-            log.info('Dry run, not saving results')
-        else:
-            link.save()
-'''
+scanner = HTMLScanner()
