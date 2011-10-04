407ebdd75c07e622a9c08663b655cdcb875bb5d1
Chris Jones <cjones@gmail.com>
2011-10-04 16:20:07 +0000

    basic layout, a few styles..

---
+++ contrib/django-memebot/gruntle/memebot/context_processors.py
@@ -0,0 +1,15 @@
+"""MemeBot context processors"""
+
+from django.contrib.sites.models import Site
+from django.conf import settings
+
+current_site = Site.objects.get_current()
+
+def site(request):
+    """Add information about the site to template contexts"""
+    return {'site_name': current_site.name,
+            'site_domain': current_site.domain,
+            'site_baseurl': settings.BASE_SITE_URL,
+            'site_copyright': settings.FEED_COPYRIGHT,
+            'site_style': settings.STYLE_NAME,
+            }
