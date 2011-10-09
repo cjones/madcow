ce9a275f04faf8af6d2b879fd115e573d18f3c9b
Chris Jones <cjones@gmail.com>
2011-10-09 16:05:07 +0000

    don't use fancy site style on 404 pages raised when digging around non-memebot areas of the site.. unnecessarily exposes the app'

--- contrib/django-memebot/gruntle/memebot/urls/root.py
+++ contrib/django-memebot/gruntle/memebot/urls/root.py
@@ -5,4 +5,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('gruntle.memebot.views.root',
         url(r'^$', 'view_index', name='root-view-index'),
         url(r'^robots.txt$', 'view_robots', name='root-view-robots'),
+
+        # roll our own 404 to get some control over it
+        url('^(?:memebot|accounts|admin)/', 'friendly_404', name='root-friendly-404'),
+        url('', 'harsh_404', name='root-harsh-404'),
         )
--- contrib/django-memebot/gruntle/memebot/views/root.py
+++ contrib/django-memebot/gruntle/memebot/views/root.py
@@ -1,6 +1,7 @@
 """View functions for root of site"""
 
 from django.views.generic.simple import direct_to_template
+from django.http import HttpResponse, Http404
 
 def view_index(request):
     """Front page"""
@@ -10,3 +11,13 @@ def view_index(request):
 def view_robots(request):
     """View the site robots.txt file"""
     return direct_to_template(request, 'root/robots.txt', mimetype='text/plain')
+
+
+def friendly_404(request):
+    """Raise 404 normally, uses 404.html template with site style"""
+    raise Http404
+
+
+def harsh_404(request):
+    """Spartan 404 that reveaals notthing about the site, used when people are digging"""
+    return HttpResponse('no.', 'text/plain', 404)
