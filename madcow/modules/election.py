5b817195b92bbe6a6d307b1faf6c072a13236d01
Chris Jones <cjones@gmail.com>
2012-07-27 22:44:40 -0700

    fix for electoral-vote horse race counter bs

--- madcow/modules/election.py
+++ madcow/modules/election.py
@@ -1,19 +1,15 @@
 """Predicted Electoral Vote Count"""
 
 import re
-from madcow.util.http import geturl
+from madcow.util.http import getsoup
 from madcow.util.color import ColorLib
-from madcow.util import Module
+from madcow.util import Module, strip_html
 
 class Main(Module):
 
     pattern = re.compile(r'^\s*(election|ev)\s*$', re.I)
     help = u'ev - current election 2008 vote prediction'
-    _baseurl = u'http://www.electoral-vote.com/'
-    _score_re = re.compile(r'<td class="score">(.*?)</td>', re.DOTALL)
-    _dem_re = re.compile(r'<span class="dem">(.*?)\s+(\d+)')
-    _gop_re = re.compile(r'<span class="gop">(.*?)\s+(\d+)')
-    _tie_re = re.compile(r'(Ties)\s+(\d+)')
+    baseurl = u'http://www.electoral-vote.com/'
 
     def init(self):
         if self.madcow is None:
@@ -22,29 +18,30 @@ class Main(Module):
             self.colorlib = self.madcow.colorlib
 
     def colorize(self, color, key, val):
-        if key == '_SEN!D':
-            key = 'Democrats'
-        elif key == '_SEN!R':
-            key = 'Republicans'
-        key = self.colorlib.get_color(color, text=key)
         return u'%s: %s' % (key, val)
 
+    def render(self, node):
+        pass
+
     def response(self, nick, args, kwargs):
-        page = geturl(self._baseurl)
-        try:
-            score = self._score_re.search(page).group(1)
-            dem = self._dem_re.search(score).groups()
-            gop = self._gop_re.search(score).groups()
-            # XXX diebold patch :D
-            #dem, gop = (dem[0], gop[1]), (gop[0], dem[1])
-            tie = None
-            try:
-                tie = self._tie_re.search(score).groups()
-            except AttributeError:
-                pass
-        except AttributeError:
-            raise Exception(u"couldn't parse page")
-        output = [self.colorize(u'blue', *dem), self.colorize(u'red', *gop)]
-        if tie:
-            output.append(self.colorize(u'white', *tie))
-        return u'%s: Projected Senate Seats 2010: %s' % (nick, u', '.join(output))
+        soup = getsoup(self.baseurl)
+        out = []
+        for box in soup.find('div', 'score-box').findAll('div', 'box'):
+            score = []
+            for key in 'name', 'score':
+                val = strip_html(box.find('span', key).renderContents()).replace(u'\xa0', u'').strip()
+                if key == 'name':
+                    if val == u'Obama':
+                        color = 'blue'
+                    elif val == 'Romney':
+                        color = 'red'
+                    else:
+                        color = None
+                    if color:
+                        val = self.colorlib.get_color(color, text=val)
+                if val:
+                    score.append(val)
+            if score:
+                out.append(u'%s: %s' % tuple(score))
+        return u'%s: %s' % (nick, u', '.join(out))
+        #from IPython.Shell import IPShellEmbed as S; #S()()
