e2112ea8ada0e25d9bb42452823cbbd79b38452d
Chris Jones <cjones@gmail.com>
2013-01-13 00:14:53 -0800

    sex up google search

--- madcow/modules/google.py
+++ madcow/modules/google.py
@@ -4,11 +4,13 @@ from madcow.util import Module
 import re
 from madcow.util.google import Google
 
+GOOGLE = '\x1b[m\x0f\x1b[1m\x1b[34mG\x1b[31mo\x1b[33mo\x1b[34mg\x1b[32ml\x1b[31me\x1b[m\x0f'
+
 class Main(Module):
 
-    pattern = re.compile(u'^\s*google\s+(.*?)\s*$')
+    pattern = re.compile(r'^(?:search|g(?:oog(?:le)?)?)\s+(.+)\s*$', re.I)
     require_addressing = True
-    help = u"google <query> - i'm feeling lucky"
+    help = u"(g[oog[le]]|search) <query> - i'm feeling lucky"
     error = u'not so lucky today..'
 
     def init(self):
@@ -16,4 +18,4 @@ class Main(Module):
 
     def response(self, nick, args, kwargs):
         query = args[0]
-        return u'%s: %s = %s' % (nick, query, self.google.lucky(query))
+        return u'{}: {}: {}'.format(nick, GOOGLE, self.google.lucky(query))
