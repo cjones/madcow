4bf2194a835c9fd56fab12e98becdba1223d977e
Chris Jones <cjones@gmail.com>
2014-01-31 20:07:16 -0800

    refactor a few things since i have the file open i might as well do something useful

--- madcow/modules/urban.py
+++ madcow/modules/urban.py
@@ -1,10 +1,19 @@
-"""Module stub"""
+"""look up offensive definitions in urban dictionary"""
 
+from urlparse import urljoin
 import re
+
 from madcow.util import Module, strip_html
 from madcow.util.http import getsoup
-from madcow.util.text import *
-from urlparse import urljoin
+from madcow.util.text import decode
+
+
+RESULTS_PER_PAGE = 7
+
+_urban_url = 'http://www.urbandictionary.com/'
+_urban_search = urljoin(_urban_url, '/define.php')
+_urban_random = urljoin(_urban_url, '/random.php')
+
 
 class Main(Module):
 
@@ -12,55 +21,39 @@ class Main(Module):
     help = 'urban <term> [#] - lookup word/phrase on urban dictionary'
     error = u'So obscure, not even urban dictionary knows it'
 
-    urban_url = 'http://www.urbandictionary.com/'
-    urban_search = urljoin(urban_url, '/define.php')
-    urban_random = urljoin(urban_url, '/random.php')
-
-    entry_re = re.compile(r'entry_\d+')
-    newline_re = re.compile(r'(?:\r\n|[\r\n])')
-
-    RESULTS_PER_PAGE = 7
-
     def response(self, nick, args, kwargs):
         query, idx = args
         try:
             if query:
                 if idx:
                     idx = int(idx)
-                response = self.lookup(query, idx)
+                res = self.lookup(query, idx)
             else:
-                response = self.random()
-        except Exception, error:
-            response = u"That doesn't even exist in urban dictionary, stop making stuff up."
-	    return str(error), type(error)
-        return u'%s: %s' % (nick, response)
+                res = self.parse(getsoup(_urban_random, referer=_urban_url))
+        except (SystemExit, KeyboardInterrupt):
+            raise
+        except:
+            res = u"That doesn't even exist in urban dictionary, stop making stuff up."
+        return u'{}: {}'.format(nick, res)
 
     def lookup(self, query, idx=None):
         """Look up term on urban dictionary"""
         if idx is None:
             idx = 1
         orig_idx = idx
-        page = int(idx / self.RESULTS_PER_PAGE)
-        idx = (idx % self.RESULTS_PER_PAGE) - 1
+        page = int(idx / RESULTS_PER_PAGE)
+        idx = (idx % RESULTS_PER_PAGE) - 1
         if idx == -1:
             idx = 6
-        soup = getsoup(self.urban_search, {'term': query, 'page': page},
-                       referer=self.urban_url)
-        return self.parse(soup, idx, page, orig_idx)
-
-    def random(self):
-        """Get a random definition"""
-        soup = getsoup(self.urban_random, referer=self.urban_url)
-        return self.parse(soup)
+        return self.parse(getsoup(_urban_search,
+                                  {'term': query, 'page': page},
+                                  referer=_urban_url,),
+                          idx, page, orig_idx)
 
     def parse(self, soup, idx=1, current_page=1, orig_idx=1):
         """Parse page for definition"""
-
-        # get definition
-	
         table = soup.body.find('div', id='content')
-        word = self.render(table.find('div', 'word'))      
-	entries = table('div', 'box')
+        entries = table('div', 'box')
         size = len(entries)
         if not size:
             raise ValueError('no results?')
@@ -68,7 +61,12 @@ class Main(Module):
             idx = size - 1
         entry = entries[idx]
 
-        # torturous logic to guess number of entries
+        # torturous logic to guess number of entries by multiplying page count
+        # with observed items per page. note that since both of these values
+        # are apparently estimates with little basis in reality, the result is
+        # complete fiction. this misfeature is actually more trouble than it
+        # is worth since all it accomplishes is confusing people, while never
+        # managing to guess correctly. that is why i am leaving it in.
         pages = soup.body.find('div', 'pagination')
         if pages is None:
             total = len(entries)
@@ -82,26 +80,27 @@ class Main(Module):
                 if page > highest:
                     highest = page
             if highest == current_page:
-                total = ((highest - 1) * self.RESULTS_PER_PAGE) + len(entries)
+                total = ((highest - 1) * RESULTS_PER_PAGE) + len(entries)
             else:
-                total = highest * self.RESULTS_PER_PAGE
+                total = highest * RESULTS_PER_PAGE
         if orig_idx > total:
             orig_idx = total
-	
-        # construct page
-        result = u'[%d/%d] %s: ' % (orig_idx, total, word)
-        result += self.render(entry.find('div', 'meaning'))
+        result = u'[{}/{}] {}: {}'.format(orig_idx, total,
+                                          render(table.find('div', 'word')),
+                                          render(entry.find('div', 'meaning')))
         try:
-            example = self.render(entry.find('div', 'example'))
-            result += u' - Example: %s' % example
+            result += u' - Example: ' + self.render(entry.find('div', 'example'))
+        except (SystemExit, KeyboardInterrupt):
+            raise
         except:
             pass
         return result
 
-    @staticmethod
-    def render(node):
-        """Render node to text"""
-        data = node.renderContents()
-        if isinstance(data, str):
-            data = decode(data, 'utf-8')
-        return Main.newline_re.sub(' ', strip_html(data)).strip()
+
+def render(node, _newline_re=re.compile(r'(?:\r\n|[\r\n])')):
+    """Render node to text"""
+    data = node.renderContents()
+    if isinstance(data, str):
+        data = decode(data, 'utf-8')
+    return _newline_re.sub(' ', strip_html(data)).strip()
+
